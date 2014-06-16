6733e6b28193f35917931bded43e4a09e134fba7
toto <toto_golden@gmx.de>
2014-06-16 17:17:59 +0200

    Update StartWizard.py
    add Userpositionwizard to startwizard

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,5 +1,6 @@
 from Wizard import wizardManager
 from Screens.WizardLanguage import WizardLanguage
+from Screens.WizardUserInterfacePositioner import UserInterfacePositionerWizard
 from Screens.VideoWizard import VideoWizard
 from Screens.Rc import Rc
 from Screens.Screen import Screen
@@ -39,4 +40,5 @@ class StartWizard(WizardLanguage, Rc):
 wizardManager.registerWizard(VideoWizard, config.misc.videowizardenabled.getValue(), priority = 0)
 wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.getValue(), priority = 1)
 wizardManager.registerWizard(StartWizard, config.misc.firstrun.getValue(), priority = 20)
-wizardManager.registerWizard(OpenWebifConfig, config.misc.firstrun.getValue(), priority = 30)
+wizardManager.registerWizard(UserInterfacePositionerWizard, config.misc.firstrun.getValue(), priority = 30)
+wizardManager.registerWizard(OpenWebifConfig, config.misc.firstrun.getValue(), priority = 40)
