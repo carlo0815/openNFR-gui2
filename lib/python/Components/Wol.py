89cef126407da22cc26a403d1b277828845e4ccd
toto <toto_golden@gmx.de>
2014-08-09 19:26:06 +0200

    Update Wol.py

--- lib/python/Components/Wol.py
+++ lib/python/Components/Wol.py
@@ -26,13 +26,6 @@ def Init():
 		config.network.wol = ConfigSelection([("disable", _("No")), ("enable", _("Yes"))], default = "disable")
 		config.network.wol.addNotifier(setWOLmode, initial_call=True)
 
-	elif SystemInfo["ETWOL"]:
-		def setWOLmode(value):
-			iwol.setWolState(config.network.wol.value)
-		iwol = WOL()
-		config.network.wol = ConfigSelection([("off", _("No")), ("on", _("Yes"))], default = "off")
-		config.network.wol.addNotifier(setWOLmode, initial_call=True)
-
 	elif SystemInfo["WakeOnLAN"] and getBoxType() in ('gbquadplus', 'gbquad', 'gb800ueplus', 'gb800seplus', 'gbipbox'):
 		def setWOLmode(value):
 			iwol.setWolState(config.network.wol.value)
