dd235be05c04e582bbebf6a7f14c5012a7e0a7c6
schleichdi2 <harry@ubuntu.(none)>
2014-12-19 13:14:26 +0100

    fix networksetup.py

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -2281,7 +2281,7 @@ class NetworkVpnLog(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		Screen.setTitle(self, _("OpenVpn Log"))
-		self.skinName = "NetworkInadynLog"
+		self.skinName = "NetworkVpnLog"
 		self['infotext'] = ScrollLabel('')
 		self.Console = Console()
 		self['actions'] = ActionMap(['WizardActions', 'ColorActions'], {'ok': self.close, 'back': self.close, 'up': self['infotext'].pageUp, 'down': self['infotext'].pageDown})
