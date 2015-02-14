ea4509fe07a918e88ff5d4eac21c754580f66861
toto <toto_golden@gmx.de>
2015-02-14 20:37:18 +0100

    Update PluginList.py

--- lib/python/Components/PluginList.py
+++ lib/python/Components/PluginList.py
@@ -1,8 +1,9 @@
 from MenuList import MenuList
-
-from Tools.Directories import resolveFilename, SCOPE_ACTIVE_SKIN
+from enigma import getDesktop
+from Tools.Directories import resolveFilename, SCOPE_ACTIVE_SKIN, defaultPaths, SCOPE_SKIN
 from Components.MultiContent import MultiContentEntryText, MultiContentEntryPixmapAlphaTest
-
+from os import path
+from Components.config import config
 from enigma import eListboxPythonMultiContent, gFont
 from Tools.LoadPixmap import LoadPixmap
 
@@ -14,15 +15,15 @@ def PluginEntryComponent(plugin, width=440):
 
 	return [
 		plugin,
-		MultiContentEntryText(pos=(120, 5), size=(width-120, 25), font=0, text=plugin.name),
-		MultiContentEntryText(pos=(120, 26), size=(width-120, 17), font=1, text=plugin.description),
+		MultiContentEntryText(pos=(120, 5), size=(width-120, 30), font=0, text=plugin.name),
+		MultiContentEntryText(pos=(120, 33), size=(width-120, 24), font=1, text=plugin.description),
 		MultiContentEntryPixmapAlphaTest(pos=(10, 5), size=(100, 40), png = png)
 	]
 
 def PluginCategoryComponent(name, png, width=440):
 	return [
 		name,
-		MultiContentEntryText(pos=(80, 5), size=(width-80, 25), font=0, text=name),
+		MultiContentEntryText(pos=(80, 5), size=(width-80, 30), font=0, text=name),
 		MultiContentEntryPixmapAlphaTest(pos=(10, 0), size=(60, 50), png = png)
 	]
 
@@ -40,15 +41,21 @@ def PluginDownloadComponent(plugin, name, version=None, width=440):
 		name += "  (" + version + ")"
 	return [
 		plugin,
-		MultiContentEntryText(pos=(80, 5), size=(width-80, 25), font=0, text=name),
-		MultiContentEntryText(pos=(80, 26), size=(width-80, 17), font=1, text=plugin.description),
+		MultiContentEntryText(pos=(80, 5), size=(width-80, 28), font=0, text=name),
+		MultiContentEntryText(pos=(80, 33), size=(width-80, 22), font=1, text=plugin.description),
 		MultiContentEntryPixmapAlphaTest(pos=(10, 0), size=(60, 50), png = png)
 	]
 
 
 class PluginList(MenuList):
 	def __init__(self, list, enableWrapAround=True):
-		MenuList.__init__(self, list, enableWrapAround, eListboxPythonMultiContent)
-		self.l.setFont(0, gFont("Regular", 20))
-		self.l.setFont(1, gFont("Regular", 14))
-		self.l.setItemHeight(50)
+		if getDesktop(0).size().width() == 1920:
+				MenuList.__init__(self, list, enableWrapAround, eListboxPythonMultiContent)
+				self.l.setFont(0, gFont("Regular", 28))
+				self.l.setFont(1, gFont("Regular", 22))
+				self.l.setItemHeight(60)
+		else:
+			MenuList.__init__(self, list, enableWrapAround, eListboxPythonMultiContent)
+			self.l.setFont(0, gFont("Regular", 20))
+			self.l.setFont(1, gFont("Regular", 14))
+			self.l.setItemHeight(50)
