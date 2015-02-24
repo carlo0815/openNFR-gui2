024f372c4bee21ceffc5a8bce502a89068b686ce
schleichdi <schleichdi2@users.noreply.github.com>
2015-02-24 18:23:07 +0100

    Update CCcamInfo.py

--- lib/python/Screens/CCcamInfo.py
+++ lib/python/Screens/CCcamInfo.py
@@ -128,14 +128,14 @@ class EGCCcamEditAddLine(ConfigListScreen,Screen):
 	
 	if self.def_mode == "EDIT":
 		  print "EDIT Mode"
-		  zrodlo = open('/etc/CCcam.cfg').readlines()
-		  cel = open('/etc/CCcam.cfg', 'w')
+		  zrodlo = open('/usr/keys/CCcam.cfg').readlines()
+		  cel = open('/usr/keys/CCcam.cfg', 'w')
 		  for s in zrodlo:
 			  cel.write(s.replace(self.def_line , line))
 		  cel.close()
 	else:
 		  print "ADD Mode"
-		  with open("/etc/CCcam.cfg", "r+") as f:
+		  with open("/usr/keys/CCcam.cfg", "r+") as f:
 		      old = f.read() # read everything in the file
 		      f.seek(0) # rewind
 		      f.write(line + "\n" + old) # write the new line before
@@ -191,7 +191,7 @@ class EGCCcamConfigEdit(Screen):
 		self.initSelectionList()
 		self.list= []
 		
-		f = open('/etc/CCcam.cfg', 'r')
+		f = open('/usr/keys/CCcam.cfg', 'r')
 		for line in f.readlines():
 			if line.startswith('C:'):
 			  print "Find C: lines in CCcam.cfg"
@@ -243,8 +243,8 @@ class EGCCcamConfigEdit(Screen):
 	      print "keyRemove2"
 	      mysel = self['menu'].getCurrent()    
 	      if mysel:
-		zrodlo = open('/etc/CCcam.cfg').readlines()
-		cel = open('/etc/CCcam.cfg', 'w')
+		zrodlo = open('/usr/keys/CCcam.cfg').readlines()
+		cel = open('/usr/keys/CCcam.cfg', 'w')
 		for s in zrodlo:
 			cel.write(s.replace(mysel , "#"))
 		cel.close()
@@ -263,7 +263,7 @@ class EGCCcamConfigEdit(Screen):
 
 VERSION = "v1.3c"
 DATE = "24.12.2009"
-CFG = "/etc/CCcam.cfg"
+CFG = "/usr/keys/CCcam.cfg"
 
 #############################################################
 
@@ -432,7 +432,7 @@ def getConfigNameAndContent(fileName):
 		idx = name.index("\n")
 		name = name[:idx]
 	else:
-		name = fileName.replace("/var/etc/", "")
+		name = fileName.replace("/usr/keys/", "")
 
 	return name, content
 
@@ -1793,13 +1793,13 @@ class CCcamInfoConfigSwitcher(Screen):
 		list = []
 
 		try:
-			files = listdir("/var/etc")
+			files = listdir("/usr/keys/")
 		except:
 			files = []
 
 		for file in files:
 			if file.startswith("CCcam_") and file.endswith(".cfg"):
-				list.append(CCcamConfigListEntry("/var/etc/"+file))
+				list.append(CCcamConfigListEntry("/usr/keys/"+file))
 
 		self["list"].setList(list)
 
