66cf295ab970ebd996f424995a455634ae0406a7
toto <toto_golden@gmx.de>
2015-03-08 18:29:24 +0100

    add Install missing Plugins

--- lib/python/Plugins/Extensions/bmediacenter/src/plugin.py
+++ lib/python/Plugins/Extensions/bmediacenter/src/plugin.py
@@ -5,10 +5,13 @@ from Components.ConfigList import ConfigList
 from Components.config import *
 from skin import loadSkin
 from Components.Label import Label
+from Components.Console import Console
 from Screens.Screen import Screen
 from Components.Pixmap import Pixmap
 from Plugins.Plugin import PluginDescriptor
 from Tools.Directories import pathExists, fileExists
+from Screens.MessageBox import MessageBox
+from Screens.Standby import TryQuitMainloop
 from __init__ import _
 import os
 import commands
@@ -38,6 +41,7 @@ class DMC_MainMenu(Screen):
 		self["left"] = Pixmap()
 		self["middle"] = Pixmap()
 		self["right"] = Pixmap()
+		self.Console = Console()
 		self.oldbmcService = self.session.nav.getCurrentlyPlayingServiceReference()
 		self.session.nav.stopService()
 		# Disable OSD Transparency
@@ -83,6 +87,50 @@ class DMC_MainMenu(Screen):
 			if fileExists("/media/upnp") is False:
 				os.mkdir("/media/upnp")
 			os.system('djmount /media/upnp &')
+
+	def checkNetworkState(self, str, retval, extra_args):
+		if 'Collected errors' in str:
+			self.session.openWithCallback(self.close, MessageBox, _("A background update check is in progress, please wait a few minutes and try again."), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
+		elif not str:
+			self.feedscheck = self.session.open(MessageBox,_('Please wait whilst feeds state is checked.'), MessageBox.TYPE_INFO, enable_input = False)
+			self.feedscheck.setTitle(_('Checking Feeds'))
+			cmd1 = "opkg update"
+			self.CheckConsole = Console()
+			self.CheckConsole.ePopen(cmd1, self.checkNetworkStateFinished)
+		else:
+			self.updateService()
+
+	def checkNetworkStateFinished(self, result, retval,extra_args=None):
+		if 'bad address' in result:
+			self.session.openWithCallback(self.InstallPackageFailed, MessageBox, _("Your %s %s is not connected to the internet, please check your network settings and try again.") % (getMachineBrand(), getMachineName()), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
+		elif ('wget returned 1' or 'wget returned 255' or '404 Not Found') in result:
+			self.session.openWithCallback(self.InstallPackageFailed, MessageBox, _("Sorry feeds are down for maintenance, please try again later."), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
+		else:
+			self.session.openWithCallback(self.InstallPackage, MessageBox, _('Ready to install %s ?') % self.service_name, MessageBox.TYPE_YESNO)
+
+	def InstallPackage(self, val):
+		if val:
+			self.doInstall(self.installComplete, self.service_name)
+		else:
+			self.feedscheck.close()
+			self.close()			
+			
+	def doInstall(self, callback, pkgname):
+		self.message = self.session.open(MessageBox,_("please wait..."), MessageBox.TYPE_INFO, enable_input = False)
+		self.message.setTitle(_('Installing Service'))
+		self.Console.ePopen('/usr/bin/opkg install ' + pkgname, callback)
+
+	def installComplete(self,result = None, retval = None, extra_args = None):
+		self.session.open(TryQuitMainloop, 3)
+
+	def InstallCheckVLC(self):
+	        self.service_name = 'enigma2-plugin-extensions-vlcplayer'
+		self.Console.ePopen('/usr/bin/opkg list_installed ' + self.service_name, self.checkNetworkState)
+
+	def InstallCheckSHOUT(self):
+	        self.service_name = 'enigma2-plugin-extensions-shoutcast'
+		self.Console.ePopen('/usr/bin/opkg list_installed ' + self.service_name, self.checkNetworkState)
+
 	def next(self):
 		self["menu"].selectNext()
 		if self["menu"].getIndex() == 1:
@@ -178,7 +226,9 @@ class DMC_MainMenu(Screen):
 					self.session.open(MC_VLCServerlist)
 				else:
 					self.session.open(MessageBox,"Error: VLC-Player Plugin not installed ...",  MessageBox.TYPE_INFO)
-			elif selection[1] == "Webbrowser":
+			                self.InstallCheckVLC()
+                        
+                        elif selection[1] == "Webbrowser":
                                 if os.path.exists("/usr/lib/enigma2/python/Plugins/Extensions/HbbTV/") == True:
                                        from Plugins.Extensions.HbbTV.plugin import OperaBrowser
 		                       global didOpen
@@ -195,7 +245,8 @@ class DMC_MainMenu(Screen):
                                         self.session.open(SHOUTcastWidget)
                                 else:
 					self.session.open(MessageBox,"Error: SHOUTcast Plugin not installed ...",  MessageBox.TYPE_INFO)                                        					
-			elif selection[1] == "MC_WeatherInfo":
+			                self.InstallCheckSHOUT()
+                        elif selection[1] == "MC_WeatherInfo":
 				from MC_WeatherInfo import MC_WeatherInfo
 				self.session.open(MC_WeatherInfo)
 			elif selection[1] == "MC_Settings":
