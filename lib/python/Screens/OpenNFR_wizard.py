cb80ab4710dc2afbf0af1e2fe043a82ecb91cf9e
toto <toto_golden@gmx.de>
2015-04-10 21:52:49 +0200

    add gui for webif

--- lib/python/Screens/OpenNFR_wizard.py
+++ lib/python/Screens/OpenNFR_wizard.py
@@ -19,6 +19,7 @@ from Screens.MessageBox import MessageBox
 from Components.Sources.StaticText import StaticText
 from Components.MenuList import MenuList
 from Components.MultiContent import MultiContentEntryText
+from Screens.Standby import *
 
 config.opennfrwizard = ConfigSubsection()
 config.opennfrwizard.enablewebinterface = ConfigYesNo(default=False)
@@ -45,22 +46,21 @@ class OpenNFRWizardSetup(ConfigListScreen, Screen):
 	list.append(getConfigListEntry(_('Enable OpenNfr Skalli Skin mod bei Blasser ?'), config.opennfrwizard.enableskalliskin))
 	list.append(getConfigListEntry(_('Enable OpenNfr MainMenu2 ?'), config.opennfrwizard.enablemainmenu2))		
 	list.append(getConfigListEntry(_('Enable HBBTV ?'), config.opennfrwizard.enablehbbtv))
-	list.append(getConfigListEntry(_('Enable 3G / 4G Modems support ?'), config.opennfrwizard.enable3gmodems))
-	list.append(getConfigListEntry(_('Enable Firmware Update Plugin ?'), config.opennfrwizard.firmwaremicom))
-	list.append(getConfigListEntry(_('Enable WiFi drivers ?'), config.opennfrwizard.enableWifiDrivers))
 
-        self["key_red"] = Label(_("Save"))
-        self["key_green"] = Label(_("Exit"))
-        self['label1'] = Label(_('Bei Install dieser Plugins kann es bei zu wenig Flashspeicher zu Problemen kommen \n\nDas Image könnte platzen!'))
+        self["key_red"] = Label(_("Exit"))
+        self["key_green"] = Label(_("Save"))
+        self['label1'] = Label(_('Bei Install dieser Plugins kann es bei zu wenig Flashspeicher zu Problemen kommen \n\nDas Image kÃ¶nnte platzen!'))
 		 
         ConfigListScreen.__init__(self, list) 
         self['actions'] = ActionMap(['OkCancelActions',
-         'ColorActions'], {'red': self.run, 'green' : self.dontSaveAndExit,
+         'ColorActions'], {'green': self.run, 'red' : self.dontSaveAndExit,
          'cancel': self.dontSaveAndExit}, -1)
 
     def run(self):
 	cmd = ""
+	webinstall = "0"
 	if config.opennfrwizard.enablewebinterface.value is True:
+	        webinstall = "1"
                 cmd += "opkg install --force-overwrite enigma2-plugin-extensions-webinterface-nfrmod;"
 	else:
 		cmd += "opkg remove --force-depends enigma2-plugin-extensions-webinterface-nfrmod;"
@@ -85,21 +85,6 @@ class OpenNFRWizardSetup(ConfigListScreen, Screen):
 	else:
 		cmd += "opkg remove --force-depends tslib-conf libts-1.0-0 libsysfs2 libgmp10 libmpfr4 vuplus-opera-browser-util enigma2-plugin-extensions-inihbbtv;"
 
-	if config.opennfrwizard.enable3gmodems.value is True:
-		cmd += "opkg install --force-overwrite enigma2-plugin-systemplugins-3gmodemmanager;"
-	else:
-		cmd += "opkg remove --force-depends ppp usbmodeswitch usbmodeswitch-data wvdial wvstreams libwvutils4.6 libwvstreams-extras libuniconf4.6 kernel-module-ppp-async kernel-module-ppp-deflate kernel-module-ppp-synctty kernel-module-ppp-generic kernel-module-slhc kernel-module-usbserial kernel-module-cdc-acm kernel-module-ppp-mppe kernel-module-pppoe kernel-module-pppox kernel-module-option kernel-module-bsd-comp usbutils enigma2-plugin-systemplugins-3gmodemmanager;"
-
-	if config.opennfrwizard.firmwaremicom.value is True:
-		cmd += "opkg install --force-overwrite enigma2-plugin-systemplugins-micomupgrade;"
-	else:
-		cmd += "opkg remove --force-depends enigma2-plugin-systemplugins-micomupgrade;"
-
-	if config.opennfrwizard.enableWifiDrivers is True:
-		cmd += "opkg install --force-overwrite enigma2-plugin-drivers-network-usb-ath9k-htc enigma2-plugin-drivers-network-usb-carl9170 enigma2-plugin-drivers-network-usb-rt2800 enigma2-plugin-drivers-network-usb-rt2500 enigma2-plugin-drivers-network-usb-rtl8187 enigma2-plugin-drivers-network-usb-smsc75xx enigma2-plugin-drivers-network-usb-zd1211rw enigma2-plugin-drivers-network-usb-rtl8192cu enigma2-plugin-drivers-network-usb-rt73 enigma2-plugin-drivers-network-usb-r8712u;"
-	else:
-		cmd += "opkg remove --force-depends enigma2-plugin-drivers-network-usb-ath9k-htc enigma2-plugin-drivers-network-usb-carl9170 enigma2-plugin-drivers-network-usb-rt2800 enigma2-plugin-drivers-network-usb-rt2500 enigma2-plugin-drivers-network-usb-rtl8187 enigma2-plugin-drivers-network-usb-smsc75xx enigma2-plugin-drivers-network-usb-zd1211rw enigma2-plugin-drivers-network-usb-rtl8192cu enigma2-plugin-drivers-network-usb-rt73 enigma2-plugin-drivers-network-usb-r8712u;"
-
         for x in self['config'].list:
             x[1].save()
 
@@ -107,7 +92,10 @@ class OpenNFRWizardSetup(ConfigListScreen, Screen):
 	self.session.open(Console, title = _("Please wait configuring OpenNFR Image"), cmdlist = [cmd], finishedCallback = None, closeOnSuccess = True)
 	plugins.readPluginList(resolveFilename(SCOPE_PLUGINS))
 
-        self.close()
+	if webinstall == "1":
+		quitMainloop(3)
+	else:
+		self.close()
 
     def dontSaveAndExit(self):
         for x in self['config'].list:
