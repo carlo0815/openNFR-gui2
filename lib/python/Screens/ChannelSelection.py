b90fd7a120bfdb352b57197c0ad9be4d217740da
toto <toto_golden@gmx.de>
2015-07-01 20:10:43 +0200

    Update ChannelSelection.py

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -479,7 +479,6 @@ class ChannelContextMenu(Screen):
 		if self.session.pipshown:
 			del self.session.pip
 		self.session.pip = self.session.instantiateDialog(PictureInPicture)
-		self.session.pip.setAnimationMode(0)
 		self.session.pip.show()
 		newservice = self.csel.servicelist.getCurrent()
 		currentBouquet = self.csel.servicelist and self.csel.servicelist.getRoot()
@@ -2330,7 +2329,6 @@ class ChannelSelectionRadio(ChannelSelectionBase, ChannelSelectionEdit, ChannelS
 		self.onLayoutFinish.append(self.onCreate)
 
 		self.info = session.instantiateDialog(RadioInfoBar) # our simple infobar
-		self.info.setAnimationMode(0)
 
 		self["actions"] = ActionMap(["OkCancelActions", "TvRadioActions"],
 			{
