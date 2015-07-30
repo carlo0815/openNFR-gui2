2f160cd06e6817a78f28729e039a8f9a7432f3ec
torwald12 <torwald12@team.nachtfalke.biz>
2015-07-30 18:26:20 +0200

    Update KeyBindings.py

--- lib/python/Tools/KeyBindings.py
+++ lib/python/Tools/KeyBindings.py
@@ -68,12 +68,12 @@ keyDescriptions = [{
 		KEYIDS["KEY_TEXT"]: ("TEXT",),
 		KEYIDS["KEY_NEXT"]: ("ARROWRIGHT",),
 		KEYIDS["KEY_PREVIOUS"]: ("ARROWLEFT",),
-		KEYIDS["KEY_PREVIOUSSONG"]: ("RED", "SHIFT"),
-		KEYIDS["KEY_PLAYPAUSE"]: ("YELLOW", "SHIFT"),
-		KEYIDS["KEY_PLAY"]: ("GREEN", "SHIFT"),
-		KEYIDS["KEY_NEXTSONG"]: ("BLUE", "SHIFT"),
-		KEYIDS["KEY_CHANNELUP"]: ("BOUQUET+",),
-		KEYIDS["KEY_CHANNELDOWN"]: ("BOUQUET-",),
+		KEYIDS["KEY_PREVIOUSSONG"]: ("PREVIOUSSONG"),
+		KEYIDS["KEY_PLAYPAUSE"]: ("PLAYPAUSE"),
+		KEYIDS["KEY_PLAY"]: ("PLAY"),
+		KEYIDS["KEY_NEXTSONG"]: ("NEXTSKIP"),
+		KEYIDS["KEY_CHANNELUP"]: ("CH+",),
+		KEYIDS["KEY_CHANNELDOWN"]: ("CH-",),
 		KEYIDS["KEY_0"]: ("0",),
 		KEYIDS["KEY_1"]: ("1",),
 		KEYIDS["KEY_2"]: ("2",),
@@ -130,6 +130,55 @@ keyDescriptions = [{
 		KEYIDS["KEY_STOP"]: ("STOP",),
 		KEYIDS["KEY_RECORD"]: ("RECORD",)
 	},
+	{       # XP1000
+		KEYIDS["BTN_0"]: ("UP", "fp"),
+		KEYIDS["BTN_1"]: ("DOWN", "fp"),
+		KEYIDS["KEY_OK"]: ("OK", ""),
+		KEYIDS["KEY_UP"]: ("UP",),
+		KEYIDS["KEY_DOWN"]: ("DOWN",),
+		KEYIDS["KEY_POWER"]: ("POWER",),
+		KEYIDS["KEY_RED"]: ("RED",),
+		KEYIDS["KEY_BLUE"]: ("BLUE",),
+		KEYIDS["KEY_GREEN"]: ("GREEN",),
+		KEYIDS["KEY_YELLOW"]: ("YELLOW",),
+		KEYIDS["KEY_MENU"]: ("MENU",),
+		KEYIDS["KEY_LEFT"]: ("LEFT",),
+		KEYIDS["KEY_RIGHT"]: ("RIGHT",),
+		KEYIDS["KEY_VIDEO"]: ("PVR",),
+		KEYIDS["KEY_INFO"]: ("INFO",),
+		KEYIDS["KEY_AUDIO"]: ("AUDIO",),
+		KEYIDS["KEY_SUBTITLE"]: ("SUBTITLE",),
+		KEYIDS["KEY_TV"]: ("TV",),
+		KEYIDS["KEY_RADIO"]: ("RADIO",),
+		KEYIDS["KEY_TEXT"]: ("TEXT",),
+		KEYIDS["KEY_NEXT"]: ("ARROWRIGHT",),
+		KEYIDS["KEY_PREVIOUS"]: ("ARROWLEFT",),
+		KEYIDS["KEY_PREVIOUSSONG"]: ("SKIPBACK",),
+		KEYIDS["KEY_REWIND"]: ("REWIND",),
+		KEYIDS["KEY_FASTFORWARD"]: ("FASTFORWARD",),
+		KEYIDS["KEY_NEXTSONG"]: ("SKIPFORWARD",),
+		KEYIDS["KEY_PLAYPAUSE"]: ("PLAYPAUSE",),
+		KEYIDS["KEY_CHANNELUP"]: ("BOUQUET+",),
+		KEYIDS["KEY_CHANNELDOWN"]: ("BOUQUET-",),
+		KEYIDS["KEY_0"]: ("0",),
+		KEYIDS["KEY_1"]: ("1",),
+		KEYIDS["KEY_2"]: ("2",),
+		KEYIDS["KEY_3"]: ("3",),
+		KEYIDS["KEY_4"]: ("4",),
+		KEYIDS["KEY_5"]: ("5",),
+		KEYIDS["KEY_6"]: ("6",),
+		KEYIDS["KEY_7"]: ("7",),
+		KEYIDS["KEY_8"]: ("8",),
+		KEYIDS["KEY_9"]: ("9",),
+		KEYIDS["KEY_EXIT"]: ("EXIT",),
+		KEYIDS["KEY_STOP"]: ("STOP",),
+		KEYIDS["KEY_RECORD"]: ("RECORD",),
+		KEYIDS["KEY_BOOKMARKS"]: ("PORTAL",),
+		KEYIDS["KEY_VMODE"]: ("VMODE",),
+		KEYIDS["KEY_PROGRAM"]: ("TIMER",),
+		KEYIDS["KEY_SLEEP"]: ("SLEEP",),
+		KEYIDS["KEY_EPG"]: ("EPG",),
+	},
 ]
 
 def addKeyBinding(domain, key, context, action, flags):
@@ -144,9 +193,15 @@ def queryKeyBinding(context, action):
 
 def getKeyDescription(key):
 	if rc_model.rcIsDefault():
-		idx = config.misc.rcused.getValue()
+		idx = config.misc.rcused.value
 	else:
-		idx = 2
+		rctype = config.plugins.remotecontroltype.rctype.value
+		if rctype == 14:	# XP1000
+			idx = 3
+		elif rctype == 18:	# F1
+			idx = 4
+		else:
+			idx = 2
 	if key in keyDescriptions[idx]:
 		return keyDescriptions[idx].get(key, [ ])
 
