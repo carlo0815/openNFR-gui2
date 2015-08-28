7f7fc20046ce3cb42d2b6924ce1141d6d924027d
torwald12 <torwald12@team.nachtfalke.biz>
2015-08-28 20:56:05 +0200

    Update plugin.py

--- lib/python/Plugins/Extensions/NFR4XBoot_mips/plugin.py
+++ lib/python/Plugins/Extensions/NFR4XBoot_mips/plugin.py
@@ -677,11 +677,11 @@ def checkkernel():
     mycheck = 0
     if not fileExists('/media/usb'):
         os.system('mkdir /media/usb')
-    if getBoxType() in ('bre2ze', 'twinboxlcd', 'odin2hybrid', 'axodin', 'sf8', 'odinm7', 'odinm6', 'evoe3hd', 'xp1000', 'xp1000mk', 'uniboxhd1','uniboxhd2', 'uniboxhd3', 'uniboxhde', 'ini-1000de', 'xpeedlxcs2', 'xpeedlxcc', 'xpeedlx2', 'xpeedlx1', 'atemio5x00', 'ini-9000de', 'xpeedlx3', 'ini-8000am', 'atemionemesis', 'vusolo', 'vusolose', 'vuduo', 'vuuno', 'vuultimo', 'vusolo2', 'vuduo2', 'mutant2400', 'quadbox2400', 'formuler1', 'formuler3', 'atemio6200', 'atemio6000'):
+    if getBoxType() in ('bre2ze', 'twinboxlcd', 'optimussos3plus', 'odin2hybrid', 'axodin', 'sf8', 'odinm7', 'odinm6', 'evoe3hd', 'xp1000', 'xp1000mk', 'uniboxhd1','uniboxhd2', 'uniboxhd3', 'uniboxhde', 'ini-1000de', 'xpeedlxcs2', 'xpeedlxcc', 'xpeedlx2', 'xpeedlx1', 'atemio5x00', 'ini-9000de', 'xpeedlx3', 'ini-8000am', 'atemionemesis', 'vusolo', 'vusolose', 'vuduo', 'vuuno', 'vuultimo', 'vusolo2', 'vuduo2', 'mutant2400', 'quadbox2400', 'formuler1', 'formuler3', 'atemio6200', 'atemio6000'):
        mycheck = 1
     elif getBoxType() in ('gb800se', 'gbquad', 'gbquadplus', 'gb800ueplus', 'gb800seplus'): 
        mycheck = 1
-    elif getBoxType() in ('iqonios300hd', 'optimussos3plus', 'starsatlx'): 
+    elif getBoxType() in ('iqonios300hd', 'starsatlx'): 
        mycheck = 2
     else:
         mycheck = 0
