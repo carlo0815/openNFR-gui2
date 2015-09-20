9e1735adf4cce995873d0e25c2a62fa97b868dfb
toto <toto_golden@gmx.de>
2015-09-20 17:53:40 +0200

    Update rcinput.cpp

--- lib/driver/rcinput.cpp
+++ lib/driver/rcinput.cpp
@@ -491,17 +491,7 @@ class eInputDeviceInit
 public:
 	eInputDeviceInit()
 	{
-		int i = 0;
-		consoleFd = ::open("/dev/tty0", O_RDWR);
-		while (1)
-		{
-			char filename[32];
-			sprintf(filename, "/dev/input/event%d", i);
-			if (::access(filename, R_OK) < 0) break;
-			add(filename);
-			++i;
-		}
-		eDebug("Found %d input devices.", i);
+		addAll();
 	}
 
 	~eInputDeviceInit()
@@ -531,6 +521,35 @@ public:
 			}
 		}
 		eDebug("Remove '%s', not found", filename);
+	}
+
+	void addAll(void)
+	{
+		int i = 0;
+		if (consoleFd < 0)
+		{
+			consoleFd = ::open("/dev/tty0", O_RDWR);
+			printf("consoleFd %d\n", consoleFd);
+		}
+		while (1)
+		{
+			char filename[32];
+			sprintf(filename, "/dev/input/event%d", i);
+			if (::access(filename, R_OK) < 0) break;
+			add(filename);
+			++i;
+		}
+		eDebug("Found %d input devices.", i);
+	}
+
+	void removeAll(void)
+	{
+		int size = items.size();
+		for (itemlist::iterator it = items.begin(); it != items.end(); ++it)
+		{
+			delete *it;
+		}
+		items.clear();
 	}
 };
 
