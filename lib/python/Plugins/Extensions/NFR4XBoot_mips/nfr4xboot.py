31fe3da96f3f43148388136ba3a407803e6f732f
toto <toto_golden@gmx.de>
2015-10-15 17:52:01 +0200

    Update nfr4xboot.py

--- lib/python/Plugins/Extensions/NFR4XBoot_mips/nfr4xboot.py
+++ lib/python/Plugins/Extensions/NFR4XBoot_mips/nfr4xboot.py
@@ -3,231 +3,160 @@ import os
 import struct
 import shutil
 
-def NFR4XBootMainEx(source, target, installsettings, zipdelete, getimagefolder, getMachineRootFile):
-    nfr4xhome = '/media/nfr4xboot'
-    nfr4xroot = 'media/nfr4xboot'
-    to = '/media/nfr4xboot/NFR4XBootI/' + target
-    cmd = 'rm -r %s > /dev/null 2<&1' % to
-    rc = os.system(cmd)
-    to = '/media/nfr4xboot/NFR4XBootI/' + target
-    cmd = 'mkdir %s > /dev/null 2<&1' % to
-    rc = os.system(cmd)
-    to = '/media/nfr4xboot/NFR4XBootI/' + target
-    cmd = 'chmod -R 0777 %s' % to
-    rc = os.system(cmd)
+media_nf = '/media/nfr4xboot'
+mediahome = media_nf + '/NFR4XBootI/'
+
+extensions_path = '/usr/lib/enigma2/python/Plugins/Extensions/'
+extensions_path_extractpy = extensions_path + 'NFR4XBoot/ubi_reader/ubi_extract_files.py'
+extensions_path_extractpyo = extensions_path_extractpy + 'o'
+dev_null = ' > /dev/null 2>&1'
+
+def NFR4XBootMainEx(source, target, installsettings, bootquest, zipdelete, getimagefolder, getMachineRootFile):
+    
+    media_nfr_target = mediahome + target
+    list_one = ['rm -r ' + media_nfr_target + dev_null,
+                'mkdir ' + media_nfr_target + dev_null, 
+                'chmod -R 0777 ' + media_nfr_target]
+    
+    for command in list_one:
+        os.system(command)
+    
     rc = NFR4XBootExtract(source, target, zipdelete, getimagefolder, getMachineRootFile)
-    cmd = 'mkdir -p %s/NFR4XBootI/%s/media > /dev/null 2>&1' % (nfr4xhome, target)
-    rc = os.system(cmd)
-    cmd = 'rm %s/NFR4XBootI/%s/%s > /dev/null 2>&1' % (nfr4xhome, target, nfr4xroot)
-    rc = os.system(cmd)
-    cmd = 'rmdir %s/NFR4XBootI/%s/%s > /dev/null 2>&1' % (nfr4xhome, target, nfr4xroot)
-    rc = os.system(cmd)
-    cmd = 'mkdir -p %s/NFR4XBootI/%s/%s > /dev/null 2>&1' % (nfr4xhome, target, nfr4xroot)
-    rc = os.system(cmd)
-    cmd = 'cp /etc/network/interfaces %s/NFR4XBootI/%s/etc/network/interfaces > /dev/null 2>&1' % (nfr4xhome, target)
-    rc = os.system(cmd)
-    cmd = 'cp /etc/passwd %s/NFR4XBootI/%s/etc/passwd > /dev/null 2>&1' % (nfr4xhome, target)
-    rc = os.system(cmd)
-    cmd = 'cp /etc/resolv.conf %s/NFR4XBootI/%s/etc/resolv.conf > /dev/null 2>&1' % (nfr4xhome, target)
-    rc = os.system(cmd)
-    cmd = 'cp /etc/wpa_supplicant.conf %s/NFR4XBootI/%s/etc/wpa_supplicant.conf > /dev/null 2>&1' % (nfr4xhome, target)
-    rc = os.system(cmd)
-    cmd = 'rm -rf %s/NFR4XBootI/%s/usr/lib/enigma2/python/Plugins/Extensions/HbbTV' % (nfr4xhome, target)
-    rc = os.system(cmd)
-    cmd = 'cp -r /usr/lib/enigma2/python/Plugins/Extensions/NFR4XBoot/NFR4XBoot_client %s/NFR4XBootI/%s/usr/lib/enigma2/python/Plugins/Extensions/ > /dev/null 2>&1' % (nfr4xhome, target)
-    rc = os.system(cmd)
-    cmd = 'cp -r /usr/lib/enigma2/python/Plugins/Extensions/NFR4XBoot/.nfr4xboot_location %s/NFR4XBootI/%s/usr/lib/enigma2/python/Plugins/Extensions/NFR4XBoot_client/.nfr4xboot_location > /dev/null 2>&1' % (nfr4xhome, target)
-    rc = os.system(cmd) 
-    if installsettings == 'True':
-        cmd = 'mkdir -p %s/NFR4XBootI/%s/etc/enigma2 > /dev/null 2>&1' % (nfr4xhome, target)
-        rc = os.system(cmd)
-        cmd = 'cp -f /etc/enigma2/* %s/NFR4XBootI/%s/etc/enigma2/' % (nfr4xhome, target)
-        rc = os.system(cmd)
-        cmd = 'cp -f /etc/tuxbox/* %s/NFR4XBootI/%s/etc/tuxbox/' % (nfr4xhome, target)
-        rc = os.system(cmd)
-    cmd = 'mkdir -p %s/NFR4XBootI/%s/media > /dev/null 2>&1' % (nfr4xhome, target)
-    rc = os.system(cmd)
-    cmd = 'mkdir -p %s/NFR4XBootI/%s/media/usb > /dev/null 2>&1' % (nfr4xhome, target)
-    rc = os.system(cmd)
-    filename = nfr4xhome + '/NFR4XBootI/' + target + '/etc/fstab'
-    filename2 = filename + '.tmp'
-    out = open(filename2, 'w')
-    f = open(filename, 'r')
-    for line in f.readlines():
-        if line.find('/dev/mtdblock2') != -1:
-            line = '#' + line
-        elif line.find('/dev/root') != -1:
-            line = '#' + line
-        out.write(line)
+    
+    list_two = ['mkdir -p ' + media_nfr_target + '/media' + dev_null,
+                'rm ' + media_nfr_target + media_nf + dev_null, 
+                'rmdir ' + media_nfr_target + media_nf + dev_null,
+                'mkdir -p ' + media_nfr_target + media_nf + dev_null,
+                'cp /etc/network/interfaces ' + media_nfr_target + '/etc/network/interfaces' + dev_null,
+                'cp /etc/passwd ' + media_nfr_target + '/etc/passwd' + dev_null,
+                'cp /etc/resolv.conf ' + media_nfr_target + '/etc/resolv.conf' + dev_null,
+                'cp /etc/wpa_supplicant.conf ' + media_nfr_target + '/etc/wpa_supplicant.conf' + dev_null,
+                'rm -rf ' + media_nfr_target + extensions_path + 'HbbTV',
+                'cp -r ' + extensions_path + 'NFR4XBoot/NFR4XBoot_client ' + media_nfr_target + extensions_path + dev_null,
+                'cp -r ' + extensions_path + 'NFR4XBoot/.nfr4xboot_location ' + media_nfr_target + extensions_path + 'NFR4XBoot_client/.nfr4xboot_location' + dev_null]
+                
+    for command in list_two:
+        os.system(command)
+    
+    if installsettings == "True":
+        
+        list_three = ['mkdir -p ' + media_nfr_target + '/etc/enigma2' + dev_null,
+                      'cp -f /etc/enigma2/* ' + media_nfr_target + '/etc/enigma2/',
+                      'cp -f /etc/tuxbox/* ' + media_nfr_target + '/etc/tuxbox/']
+        for command in list_three:
+            os.system(command)
+        
+    os.system('mkdir -p ' + media_nfr_target + '/media/usb' + dev_null)
+    
+    list_four = ['/etc/fstab', '/usr/lib/enigma2/python/Components/config.py', '/usr/lib/enigma2/python/Tools/HardwareInfoVu.py']
+    for entrie in list_four:
+        filename = media_nfr_target + entrie
+        tempfile = filename + '.tmp'
+        if os.path.exists(filename):
+                out = open(tempfile, 'w')
+                f = open(filename, 'r')
+                for line in f.readlines():
+                  if '/etc/fstab' in entrie:  
+                     if '/dev/mtdblock2' in line:
+                        line = '#' + line
+                     elif '/dev/root' in line:
+                        line = '#' + line
+                  if 'config.py' in entrie:  
+                     if 'if file("/proc/stb/info/vumodel")' in line:
+                        line = '#' + line
+                     elif 'rckeyboard_enable = True' in line:
+                        line = '#' + line
+                  if 'HardwareInfoVu.py' in entrie:  
+                     if 'print "hardware detection failed"' in line:
+                        line = '\t\t    HardwareInfoVu.device_name ="duo"'
+          
+                  out.write(line)
 
-    f.close()
-    out.close()
-    os.rename(filename2, filename)
-    tpmd = nfr4xhome + '/NFR4XBootI/' + target + '/etc/init.d/tpmd'
-    if os.path.exists(tpmd):
-        os.system('rm ' + tpmd)
-    filename = nfr4xhome + '/NFR4XBootI/' + target + '/usr/lib/enigma2/python/Components/config.py'
-    if os.path.exists(filename):
-        filename2 = filename + '.tmp'
-        out = open(filename2, 'w')
-        f = open(filename, 'r')
-        for line in f.readlines():
-            if line.find('if file("/proc/stb/info/vumodel")') != -1:
-                line = '#' + line
-            elif line.find('rckeyboard_enable = True') != -1:
-                line = '#' + line
-            out.write(line)
-
-        f.close()
-        out.close()
-        os.rename(filename2, filename)
-    filename = nfr4xhome + '/NFR4XBootI/' + target + '/usr/lib/enigma2/python/Tools/HardwareInfoVu.py'
-    if os.path.exists(filename):
-        filename2 = filename + '.tmp'
-        out = open(filename2, 'w')
-        f = open(filename, 'r')
-        for line in f.readlines():
-            if line.find('print "hardware detection failed"') != -1:
-                line = '\t\t    HardwareInfoVu.device_name ="duo"'
-            out.write(line)
-
-        f.close()
-        out.close()
-        os.rename(filename2, filename)
-    filename = nfr4xhome + '/NFR4XBootI/' + target + '/etc/bhversion'
-    if os.path.exists(filename):
-        os.system('echo "BlackHole 2.0.9" > ' + filename)
-    mypath = nfr4xhome + '/NFR4XBootI/' + target + '/usr/lib/opkg/info/'
-    if not os.path.exists(mypath):
-        mypath = nfr4xhome + '/NFR4XBootI/' + target + '/var/lib/opkg/info/'
-    for fn in os.listdir(mypath):
-        if fn.find('kernel-image') != -1 and fn.find('postinst') != -1:
-            filename = mypath + fn
-            filename2 = filename + '.tmp'
-            out = open(filename2, 'w')
-            f = open(filename, 'r')
-            for line in f.readlines():
-                if line.find('/boot') != -1:
-                    line = line.replace('/boot', '/boot > /dev/null 2>\\&1; exit 0')
-                out.write(line)
-
-            if f.close():
+                f.close()
                 out.close()
-                os.rename(filename2, filename)
-                cmd = 'chmod -R 0755 %s' % filename
-                rc = os.system(cmd)
-        if fn.find('-bootlogo.postinst') != -1:
-            filename = mypath + fn
-            filename2 = filename + '.tmp'
-            out = open(filename2, 'w')
-            f = open(filename, 'r')
-            for line in f.readlines():
-                if line.find('/boot') != -1:
-                    line = line.replace('/boot', '/boot > /dev/null 2>\\&1; exit 0')
-                out.write(line)
+                os.rename(tempfile, filename)
+    
+    tpmd = media_nfr_target + '/etc/init.d/tpmd'
+    if os.path.exists(tpmd):
+       os.remove(tpmd)
+    
+    
+    filename = media_nfr_target + '/etc/bhversion'
+    if os.path.exists(filename):
+       os.system('echo "BlackHole 2.0.9" > ' + filename)
+    
+    mypath = media_nfr_target + '/usr/lib/opkg/info/'
+    if not os.path.exists(mypath):
+       mypath = media_nfr_target + '/var/lib/opkg/info/'
+        
+    for file_name in os.listdir(mypath):
+        
+        list_five = ['-bootlogo.postinst', '-bootlogo.postrm', '-bootlogo.preinst', '-bootlogo.prerm']
+        
+        for entrie in list_five:
+            if entrie in file_name or (('kernel-image' in file_name) and ('postinst' in file_name)):
+               filename = mypath + file_name
+               tempfile = filename + '.tmp'
+               out = open(tempfile, 'w')
+               f = open(filename, 'r')
+               for line in f.readlines():
+                   if '/boot' in line:
+                      line = line.replace('/boot', '/boot > /dev/null 2>\\&1; exit 0')
+                   out.write(line)
 
-            f.close()
-            out.close()
-            os.rename(filename2, filename)
-            cmd = 'chmod -R 0755 %s' % filename
-            rc = os.system(cmd)
-        if fn.find('-bootlogo.postrm') != -1:
-            filename = mypath + fn
-            filename2 = filename + '.tmp'
-            out = open(filename2, 'w')
-            f = open(filename, 'r')
-            for line in f.readlines():
-                if line.find('/boot') != -1:
-                    line = line.replace('/boot', '/boot > /dev/null 2>\\&1; exit 0')
-                out.write(line)
-
-            f.close()
-            out.close()
-            os.rename(filename2, filename)
-            cmd = 'chmod -R 0755 %s' % filename
-            rc = os.system(cmd)
-        if fn.find('-bootlogo.preinst') != -1:
-            filename = mypath + fn
-            filename2 = filename + '.tmp'
-            out = open(filename2, 'w')
-            f = open(filename, 'r')
-            for line in f.readlines():
-                if line.find('/boot') != -1:
-                    line = line.replace('/boot', '/boot > /dev/null 2>\\&1; exit 0')
-                out.write(line)
-
-            f.close()
-            out.close()
-            os.rename(filename2, filename)
-            cmd = 'chmod -R 0755 %s' % filename
-            rc = os.system(cmd)
-        if fn.find('-bootlogo.prerm') != -1:
-            filename = mypath + fn
-            filename2 = filename + '.tmp'
-            out = open(filename2, 'w')
-            f = open(filename, 'r')
-            for line in f.readlines():
-                if line.find('/boot') != -1:
-                    line = line.replace('/boot', '/boot > /dev/null 2>\\&1; exit 0')
-                out.write(line)
-
-            f.close()
-            out.close()
-            os.rename(filename2, filename)
-            cmd = 'chmod -R 0755 %s' % filename
-            rc = os.system(cmd)
+               f.close()
+               out.close()
+               os.rename(tempfile, filename)
+               os.chmod(filename, 0755)
 
     rc = NFR4XBootRemoveUnpackDirs(getimagefolder)
-    filename = nfr4xhome + '/NFR4XBootI/.nfr4xboot'
-    out = open('/media/nfr4xboot/NFR4XBootI/.nfr4xboot', 'w')
-    out.write(target)
-    out.close()
-    os.system('touch /tmp/.opennfrreboot')
-    rc = os.system('sync')
-    os.system('reboot -p')
-
+    if bootquest == "True":
+        out = open(mediahome + '.nfr4xboot', 'w')
+        out.write(target)
+        out.close()
+        os.system('touch /tmp/.opennfrreboot; sync; reboot -p')        
+    else: 
+        out = open(mediahome + '.nfr4xboot', 'w')
+        out.write(target)
+        out.close()    
+        os.system('echo "[NFR4XBoot] Image-Install ready, Image starts by next booting, please push exit!"')
+    
 
 def NFR4XBootRemoveUnpackDirs(getimagefolder):
-    os.chdir('/media/nfr4xboot/NFR4XBootUpload')
-    if os.path.exists('/media/nfr4xboot/NFR4XBootUpload/%s'% getimagefolder):
-        shutil.rmtree('%s'% getimagefolder)
+    os.chdir(media_nf + '/NFR4XBootUpload')
+    if os.path.exists(media_nf + '/NFR4XBootUpload/%s'% getimagefolder):
+       shutil.rmtree('%s'% getimagefolder)
  
 
 def NFR4XBootExtract(source, target, zipdelete, getimagefolder, getMachineRootFile):
     NFR4XBootRemoveUnpackDirs(getimagefolder)
-    os.system('rm -rf /media/nfr4xboot/ubi')
-    if os.path.exists('/media/nfr4xboot/ubi') is False:
-        rc = os.system('mkdir /media/nfr4xboot/ubi')
-    sourcefile = '/media/nfr4xboot/NFR4XBootUpload/%s.zip' % source
-    if os.path.exists(sourcefile) is True:
-        os.chdir('/media/nfr4xboot/NFR4XBootUpload')
+    os.system('rm -rf ' + media_nf + '/ubi')
+    if not os.path.exists(media_nf + '/ubi'):
+       os.system('mkdir ' + media_nf + '/ubi')
+    sourcefile = media_nf + '/NFR4XBootUpload/%s.zip' % source
+    if os.path.exists(sourcefile):
+        os.chdir(media_nf + '/NFR4XBootUpload')
         os.system('echo "[NFR4XBoot] Extracking ZIP image file"')
-        rc = os.system('unzip ' + sourcefile)
+        os.system('unzip ' + sourcefile)
         if zipdelete == "True":
-                rc = os.system('rm -rf ' + sourcefile)
+           os.system('rm -rf ' + sourcefile)
         else:
-                os.system('echo "[NFR4XBoot] keep  %s for next time"'% sourcefile) 
-        if os.path.exists('/media/nfr4xboot/NFR4XBootUpload/%s'% getimagefolder):
-            os.chdir('%s'% getimagefolder)
-            os.system('mv %s rootfs.bin'% getMachineRootFile)
-      
+           os.system('echo "[NFR4XBoot] keep  %s for next time"'% sourcefile) 
+        if os.path.exists(media_nf + '/NFR4XBootUpload/%s'% getimagefolder):
+           os.chdir('%s'% getimagefolder)
+           os.system('mv %s rootfs.bin'% getMachineRootFile)
 				
         print '[NFR4XBoot] Extracting UBIFS image and moving extracted image to our target'
-        if os.path.exists('/usr/lib/enigma2/python/Plugins/Extensions/NFR4XBoot/ubi_reader/ubi_extract_files.pyo'):
-            cmd = 'chmod 777 /usr/lib/enigma2/python/Plugins/Extensions/NFR4XBoot/ubi_reader/ubi_extract_files.pyo'
-            rc = os.system(cmd)
-            cmd = 'python /usr/lib/enigma2/python/Plugins/Extensions/NFR4XBoot/ubi_reader/ubi_extract_files.pyo rootfs.bin -o /media/nfr4xboot/ubi'
+        if os.path.exists(extensions_path_extractpyo):
+            os.chmod(extensions_path_extractpyo, 0777)
+            cmd = 'python ' + extensions_path_extractpyo + ' rootfs.bin -o ' + media_nf + '/ubi'
         else:
-            cmd = 'chmod 777 /usr/lib/enigma2/python/Plugins/Extensions/NFR4XBoot/ubi_reader/ubi_extract_files.py'
-            rc = os.system(cmd)
-            cmd = 'python /usr/lib/enigma2/python/Plugins/Extensions/NFR4XBoot/ubi_reader/ubi_extract_files.py rootfs.bin -o /media/nfr4xboot/ubi'        
+            os.chmod(extensions_path_extractpy, 0777)
+            cmd = 'python ' + extensions_path_extractpy + ' rootfs.bin -o ' + media_nf + '/ubi'        
         print cmd
-        rc = os.system(cmd)
+        os.system(cmd)
         os.chdir('/home/root')
-        cmd = 'cp -r -p /media/nfr4xboot/ubi/rootfs/* /media/nfr4xboot/NFR4XBootI/' + target
-        rc = os.system(cmd)
-        cmd = 'chmod -R +x /media/nfr4xboot/NFR4XBootI/' + target
-        rc = os.system(cmd)
-        cmd = 'rm -rf /media/nfr4xboot/ubi'
-        rc = os.system(cmd)
-    return 1
+        os.system('cp -r -p ' + media_nf + '/ubi/rootfs/* ' + media_nf + '/NFR4XBootI/' + target)
+        os.system('chmod -R +x ' + media_nf + '/NFR4XBootI/' + target)
+        os.system('rm -rf ' + media_nf + '/ubi')
+    return 1 
