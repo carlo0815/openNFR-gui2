95ca974627b75377c97bc3dc3b758195f049352a
NFR-Blasser <stinkebolle@gmail.com>
2015-10-29 16:58:58 +0100

    fix Ftp screen

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -1919,7 +1919,7 @@ class NetworkFtp(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		Screen.setTitle(self, _("FTP Setup"))
-		self.skinName = "NetworkSamba"
+		self.skinName = "NetworkFtp"
 		self.onChangedEntry = [ ]
 		self['lab1'] = Label(_("Autostart:"))
 		self['labactive'] = Label(_(_("Disabled")))
