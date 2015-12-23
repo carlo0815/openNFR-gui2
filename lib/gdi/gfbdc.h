f6fd1aa64cf44145af3265b73fc427963d25226c
toto <toto_golden@gmx.de>
2015-12-23 20:10:18 +0100

    OSDANIMATION Part4

--- lib/gdi/gfbdc.h
+++ lib/gdi/gfbdc.h
@@ -5,6 +5,8 @@
 #include "gpixmap.h"
 #include "gmaindc.h"
 
+#ifndef SWIG
+
 class gFBDC: public gMainDC
 {
 	fbClass *fb;
@@ -34,5 +36,10 @@ public:
 	virtual ~gFBDC();
 	int islocked() const { return fb->islocked(); }
 };
+#endif
+#ifdef HAVE_OSDANIMATION
+void setAnimation_current(int a);
+void setAnimation_speed(int speed);
+#endif
 
 #endif
