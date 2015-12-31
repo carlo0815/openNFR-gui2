5dec235a71da6946bf751425001134ed58d07ada
carlo0815 <toto_golden@gmx.de>
2015-12-31 15:58:33 +0100

    try nfr4xboot 4k

---
+++ lib/python/Plugins/Extensions/NFR4XBoot_4k/ex_init.py
@@ -0,0 +1,6 @@
+import sys, nfr4xboot
+if len(sys.argv) < 7:
+    pass
+else:
+    nfr4xboot.NFR4XBootMainEx(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+
