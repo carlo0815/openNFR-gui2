a7f17685e8a09c144bc91247e128ae8aa2c7723f
toto <toto_golden@gmx.de>
2016-06-13 17:14:11 +0200

    Update fastscan.cpp

--- lib/dvb/fastscan.cpp
+++ lib/dvb/fastscan.cpp
@@ -438,18 +438,15 @@ void eFastScan::fillBouquet(eBouquet *bouquet, std::map<int, eServiceReferenceDV
 		int number = 1;
 		for (std::map<int, eServiceReferenceDVB>::const_iterator
 			service(numbered_channels.begin()); service != numbered_channels.end(); ++service)
-			eDebug("eFastScan::Servicefill %d", service);
 		{
 			if (originalNumbering)
 			{
 				while (number < service->first)
 				{
-					m_dvb_service->m_provider_name;
 					eServiceReference ref(eServiceReference::idDVB, eServiceReference::isMarker | eServiceReference::isNumberedMarker);
 					ref.setName("-");
-					eDebug("eFastScan::eServiceReference %d", eServiceReference);
+					eDebug("eFastScan::eServiceReference %d", eServiceReference ref);
 					eDebug("eFastScan::reffill %d", ref);
-					eDebug("eFastScan::m_provider_name %d", m_provider_name);
 					bouquet->m_services.push_back(ref);
 					number++;
 				}
