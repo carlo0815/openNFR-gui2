7da1625ede83a78a437dbb58822842a1f4f60972
torwald12 <torwald12@team.nachtfalke.biz>
2016-10-05 16:03:40 +0200

    Add pip hd51

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2394,7 +2394,7 @@ class InfoBarPiP:
 								f.close()
 						self.session.pipshown = False
 				else:
-					if int(xres) <= 720 or about.getCPUString() == 'BCM7346B2' or about.getCPUString() == 'BCM7425B2' or about.getCPUString() == 'BCM7429B0' or getBoxType() in ('vusolo4k'):
+					if int(xres) <= 720 or about.getCPUString() == 'BCM7346B2' or about.getCPUString() == 'BCM7425B2' or about.getCPUString() == 'BCM7429B0' or getBoxType() in ('vusolo4k', 'mutant51', 'ax51'):
 						self.session.pip = self.session.instantiateDialog(PictureInPicture)
 						self.session.pip.setAnimationMode(0)
 						self.session.pip.show()
