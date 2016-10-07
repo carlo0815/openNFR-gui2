8c54813797f08c1cbdc21e0845c3c78e63b53150
torwald12 <torwald12@team.nachtfalke.biz>
2016-10-07 17:07:21 +0200

    Update About.py

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -144,8 +144,15 @@ def getChipSetString():
 def getCPUSpeedString():
 	if getMachineBuild() in ('vusolo4k'):
 		return "1,5 GHz"
-	elif getMachineBuild() in ('hd52', 'hd51'):
-		return "1,7 GHz"
+	elif getMachineBuild() in ('hd51','hd52'):
+		try:
+			import binascii
+			f = open('/sys/firmware/devicetree/base/cpus/cpu@0/clock-frequency', 'rb')
+			clockfrequency = f.read()
+			f.close()
+			return "%s MHz" % str(round(int(binascii.hexlify(clockfrequency), 16)/1000000,1))
+		except:
+			return "1,7 GHz"
 	else:
 		try:
 			file = open('/proc/cpuinfo', 'r')
