e699e00910437880a042799628cb0aa142cd8750
torwald12 <torwald12@team.nachtfalke.biz>
2016-11-05 21:12:06 +0100

    add sf4008

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -42,7 +42,7 @@ class About(Screen):
 		cpuMHz = ""
 		if getMachineBuild() in ('vusolo4k'):
 			cpuMHz = "   (1,5 GHz)"
-		elif getMachineBuild() in ('hd52','hd51'):
+		elif getMachineBuild() in ('hd52','hd51','sf4008'):
                         try:
 				import binascii
 				f = open('/sys/firmware/devicetree/base/cpus/cpu@0/clock-frequency', 'rb')
