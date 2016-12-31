badd6a013447ba9f462a54c838900cecadd65d8c
toto <toto_golden@gmx.de>
2016-12-31 16:59:10 +0100

    fix netscanbug

--- lib/python/Plugins/SystemPlugins/NetworkBrowser2/__init__.py
+++ lib/python/Plugins/SystemPlugins/NetworkBrowser2/__init__.py
@@ -1,9 +1,22 @@
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
 import os
+from os.path import isfile
 import gettext
+import platform
+archcheck = platform.uname()
+
 PluginLanguageDomain = 'NetworkBrowser'
 PluginLanguagePath = 'SystemPlugins/NetworkBrowser/locale'
+scantest = '/usr/lib/enigma2/python/Plugins/SystemPlugins/NetworkBrowser/netscantest'
+if isfile(scantest):
+    os.system("rm -f /usr/lib/enigma2/python/Plugins/SystemPlugins/NetworkBrowser/netscan.so")
+    if archcheck[4] == "mips":
+        os.system("rm -f /usr/lib/enigma2/python/Plugins/SystemPlugins/NetworkBrowser/netscantest")
+        os.system("cp /usr/lib/enigma2/python/Plugins/SystemPlugins/NetworkBrowser/netscan/netscan.so.mips /usr/lib/enigma2/python/Plugins/SystemPlugins/NetworkBrowser/netscan.so")      
+    elif archcheck[4] == "arm":
+        os.system("rm -f /usr/lib/enigma2/python/Plugins/SystemPlugins/NetworkBrowser/netscantest")
+        os.system("cp /usr/lib/enigma2/python/Plugins/SystemPlugins/NetworkBrowser/netscan/netscan.so.arm /usr/lib/enigma2/python/Plugins/SystemPlugins/NetworkBrowser/netscan.so ")
 
 def localeInit():
     gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
@@ -19,4 +32,3 @@ def _(txt):
 
 
 language.addCallback(localeInit())
-
