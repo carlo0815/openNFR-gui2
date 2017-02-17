66dd1e91d0e970e6f1507cdaf18eed0fb28437a3
toto <toto_golden@gmx.de>
2017-02-17 21:08:29 +0100

    Update RecordState.py

--- lib/python/Components/Sources/RecordState.py
+++ lib/python/Components/Sources/RecordState.py
@@ -1,8 +1,6 @@
 from Source import Source
 from Components.Element import cached
-from enigma import iRecordableService, pNavigation
-import Components.RecordingConfig
-from Components.config import config
+from enigma import iRecordableService
 
 class RecordState(Source):
 	def __init__(self, session):
@@ -15,7 +13,7 @@ class RecordState(Source):
 	def gotRecordEvent(self, service, event):
 		prev_records = self.records_running
 		if event in (iRecordableService.evEnd, iRecordableService.evStart, None):
-			recs = self.session.nav.getRecordings(False,Components.RecordingConfig.recType(config.recording.show_rec_symbol_for_rec_types.getValue()))
+			recs = self.session.nav.getRecordings()
 			self.records_running = len(recs)
 			if self.records_running != prev_records:
 				self.changed((self.CHANGED_ALL,))
