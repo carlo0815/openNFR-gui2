63b2da3530b5e6101231847d1e44e55312586a9e
torwald12 <torwald12@team.nachtfalke.biz>
2017-03-11 20:33:15 +0100

    add cpu speed formuler1 + treiplex

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -42,6 +42,8 @@ class About(Screen):
 		cpuMHz = ""
 		if getMachineBuild() in ('vusolo4k'):
 			cpuMHz = "   (1,5 GHz)"
+		elif getMachineBuild() in ('formuler1', 'triplex'):
+			cpuMHz = "   (1,3 GHz)"
 		elif getMachineBuild() in ('vuuno4k','vuultimo4k'):
 			cpuMHz = "   (1,7 GHz)"
 		elif getMachineBuild() in ('et1x000','hd52','hd51','sf4008'):
