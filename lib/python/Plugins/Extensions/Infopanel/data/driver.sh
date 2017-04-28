cd6cbac4d04d9206e9ba0896b205b343ff21c5c1
toto <toto_golden@gmx.de>
2017-04-28 14:32:33 +0200

    Update driver.sh

--- lib/python/Plugins/Extensions/Infopanel/data/driver.sh
+++ lib/python/Plugins/Extensions/Infopanel/data/driver.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 kernel=`uname -r`
+if ! [ -d "/etc/extraci" ] ; then
+	cp -r /lib/modules/$kernel/extra /etc
+	mv /etc/extra /etc/extraci
+	echo copy extraci
+	# ...
+fi
+if ! [  -f "/etc/modules-load.d/_formuler1.conf_ci" ] ; then
+	cp -r /etc/modules-load.d/_formuler1.conf /etc/modules-load.d/_formuler1.conf_ci
+	echo copy modload
+fi
 case "$1" in
 tcdriver)
 	echo -n "enable tc Driver and reboot...\n "
         rm /etc/modules-load.d/_formuler1.conf
         cp /etc/modules-load.d/_formuler1.conf_tc /etc/modules-load.d/_formuler1.conf
         rm -r /lib/modules/$kernel/extra
-        cp -r /lib/modules/$kernel/extratc /lib/modules/$kernel/extra        
+        cp -r /etc/extratc /lib/modules/$kernel/extra        
 	echo -e "done.\n"
 	;;
 
@@ -15,7 +25,7 @@ cidriver)
         rm /etc/modules-load.d/_formuler1.conf
         cp /etc/modules-load.d/_formuler1.conf_ci /etc/modules-load.d/_formuler1.conf
         rm -r /lib/modules/$kernel/extra
-        cp -r /lib/modules/$kernel/extraci /lib/modules/$kernel/extra               
+        cp -r /etc/extraci /lib/modules/$kernel/extra               
 	echo -e "done.\n"
 	;;
 esac
