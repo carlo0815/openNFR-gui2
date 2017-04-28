d2b0432b99dbec29fdc3b352acfede3ca0ce175d
toto <toto_golden@gmx.de>
2017-04-28 15:18:27 +0200

    Update SystemInfo.py

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -44,6 +44,8 @@ SystemInfo["FanPWM"] = SystemInfo["Fan"] and fileCheck("/proc/stb/fp/fan_pwm")
 SystemInfo["StandbyPowerLed"] = fileExists("/proc/stb/power/standbyled")
 SystemInfo["LEDButtons"] = getBoxType() == 'vuultimo'
 SystemInfo["loadcidriver"] = getBoxType() == 'formuler1'
+SystemInfo["loadci3driver"] = getBoxType() == 'formuler3'
+SystemInfo["loadci4driver"] = getBoxType() == 'formuler4'
 SystemInfo["WakeOnLAN"] = fileCheck("/proc/stb/power/wol") or fileCheck("/proc/stb/fp/wol")
 if getBoxType() in ('gbquad', 'gbquadplus','gb800ueplus', 'gb800seplus', 'gbipbox'):
 	SystemInfo["WOL"] = False
