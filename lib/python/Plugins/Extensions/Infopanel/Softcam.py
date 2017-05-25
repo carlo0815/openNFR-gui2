b6eeb4621f601be32e5112ce7149f35c2bcbe5a5
torwald12 <torwald12@team.nachtfalke.biz>
2017-05-25 20:15:13 +0200

    revert last commit

--- lib/python/Plugins/Extensions/Infopanel/Softcam.py
+++ lib/python/Plugins/Extensions/Infopanel/Softcam.py
@@ -103,7 +103,7 @@ def getcamcmd(cam):
 				print "Symlink exist to start gbox"
 			else:
 				print "Create Symlink to start gbox"
-				os.system("ln -sf 'ld-2.24.so' '/lib/ld-linux.so.3'")		
+				os.system("ln -sf '/lib/ld-2.24.so' '/lib/ld-linux.so.3'")		
  		elif "scam" in camname and not "oscam" in camname:
 			return config.NFRSoftcam.camdir.value + "/" + cam + " -s " + \
 				config.NFRSoftcam.camconfig.value + "/"			
