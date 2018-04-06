c634dc70270cfdcfe1542e8e8cfa6c8bbb8ef550
toto <toto_golden@gmx.de>
2018-04-06 22:11:47 +0200

    Add files via upload

--- lib/python/Plugins/Extensions/Infopanel/PluginWizard.py
+++ lib/python/Plugins/Extensions/Infopanel/PluginWizard.py
@@ -874,3 +874,380 @@ class PluginDeinstall(Screen):
 		self["list"].l.setList(list)
 		self["text"] = Label(_("Downloading plugin information complete."))
 			
+class PluginSkinInstall(Screen):
+	skin = """
+               <screen name="PluginInstall" position="80,160" size="1100,450" title="Installiere OpenNFRSkins">
+				<widget name="list" position="5,0" size="560,300" itemHeight="49" foregroundColor="white" backgroundColor="black" transparent="1" scrollbarMode="showOnDemand" zPosition="2" enableWrapAround="1" />
+				<widget name="status" position="580,43" size="518,300" font="Regular;16" halign="center" noWrap="1" transparent="1" />
+				<eLabel name="" position="580,6" size="517,30" font="Regular; 22" text="Liste der zu Installierenden Plugins" zPosition="3" halign="center" />
+				<widget name="text" position="580,345" size="519,60" zPosition="1" font="Regular;20" halign="center" valign="center" foregroundColor="green" transparent="1" />
+				<widget name="key_green" render="Label" position="46,366" zPosition="2" size="190,22" valign="center" halign="left" font="Regular;21" transparent="1" backgroundColor="foreground" />
+				<ePixmap position="5,365" size="35,27" pixmap="/usr/share/enigma2/skin_default/buttons/key_green.png" alphatest="blend" zPosition="2" />
+				<widget name="key_blue" render="Label" position="360,366" zPosition="2" size="190,22" valign="center" halign="left" font="Regular;21" transparent="1" backgroundColor="foreground" />
+				<ePixmap position="320,365" size="35,27" pixmap="/usr/share/enigma2/skin_default/buttons/key_blue.png" alphatest="blend" zPosition="2" />
+				<eLabel name="new eLabel" position="570,0" size="2,400" zPosition="5" foregroundColor="unc0c0c0" backgroundColor="darkgrey" />
+				<eLabel name="spaceused" text="% Flash Used..." position="45,414" size="150,20" font="Regular;19" halign="left" foregroundColor="white" backgroundColor="black" transparent="1" zPosition="5" />
+				<widget name="spaceused" position="201,415" size="894,20" foregroundColor="white" backgroundColor="blue" zPosition="3" />
+			</screen>"""
+		  
+	DOWNLOAD = 0
+	PLUGIN_PREFIX = 'enigma2-plugin-opennfrskins-'
+	lastDownloadDate = None
+
+	def __init__(self, session, type = 0, needupdate = True):
+		Screen.__init__(self, session)
+                global pluginfiles
+		self.type = type
+		self.needupdate = needupdate
+		self.container = eConsoleAppContainer()
+		self.container.appClosed.append(self.runFinished)
+		self.container.dataAvail.append(self.dataAvail)
+		self.onLayoutFinish.append(self.startRun)
+		self.onShown.append(self.setWindowTitle)
+                self.setuplist = []
+
+		self.list = []
+		self["list"] = PluginList(self.list)
+		self.pluginlist = []
+		self.expanded = []
+		self.installedplugins = []
+		self.plugins_changed = False
+		self.reload_settings = False
+		self.check_settings = False
+		self.check_bootlogo = False
+		self.install_settings_name = ''
+		self.remove_settings_name = ''
+		self['spaceused'] = ProgressBar()		
+                self["status"] = ScrollLabel()
+		self['key_green']  = Label(_('Install'))	
+		self['key_blue']  = Label(_('Exit'))
+		
+		if self.type == self.DOWNLOAD:
+			self["text"] = Label(_("Downloading plugin information. Please wait..."))
+		self.run = 0
+		self.remainingdata = ""
+		self["actions"] = ActionMap(["WizardActions", "ColorActions"],
+		{
+			"ok": self.go,
+			"back": self.requestClose,
+			"green": self.install,
+			"blue": self.close,
+		})
+		if os.path.isfile('/usr/bin/opkg'):
+			self.ipkg = '/usr/bin/opkg'
+			self.ipkg_install = self.ipkg + ' install --force-overwrite'
+			self.ipkg_install1 = self.ipkg_install + ' --force-depends'
+		else:
+			self.ipkg = 'ipkg'
+			self.ipkg_install = 'ipkg install --force-overwrite -force-defaults'
+			self.ipkg_install1 = self.ipkg_install + ' --force-depends'
+
+	def go(self):
+		sel = self["list"].l.getCurrentSelection()
+		if sel is None:
+			return
+                
+		sel = sel[0]
+		print "selection", sel
+		if isinstance(sel, str): # category
+
+			if sel in self.expanded:
+			        
+				self.expanded.remove(sel)
+			else:
+				self.expanded.append(sel)
+			self.updateList()
+			
+		else:
+		        pluginfiles = ""
+			if self.type == self.DOWNLOAD:
+			        if sel.name in self.setuplist:
+                                        self.setuplist.remove("%s" % sel.name)
+                                        if not self.setuplist:
+                                               pluginfiles += "no Plugin select"
+                                               self.listplugininfo(pluginfiles)
+                                        else:
+                                               print "Setupliste", self.setuplist 
+                                               list = self.setuplist
+                                               for item in list:
+                                                      pluginfiles += item
+                 	                              pluginfiles += "\n" 
+                 	                              print "pluginfile1:", pluginfiles
+                 	                              self.listplugininfo(pluginfiles)
+                                                      self.list = []                                                 
+			        else:
+ 			                self.setuplist.append("%s" % sel.name)
+     			                print "Setupliste", self.setuplist
+                                        list = self.setuplist
+                                        for item in list:
+                 	                       pluginfiles += item
+                 	                       pluginfiles += "\n"
+                 	                       print "pluginfile1:", pluginfiles
+                 	                       self.listplugininfo(pluginfiles)
+                                               self.list = []    			                
+
+	def install(self):
+	        PLUGIN_PREFIX = 'enigma2-plugin-'
+		cmdList = []
+		for item in self.setuplist:
+			cmdList.append((IpkgComponent.CMD_INSTALL, { "package": PLUGIN_PREFIX + item }))
+		self.session.open(Ipkg_1, cmdList = cmdList)
+                
+
+	def listplugininfo(self, pluginfiles):
+		try:
+		        pluginfiles.split("/n")	
+		        self["status"].setText(pluginfiles)                                
+		except:
+			self["status"].setText("")
+
+
+
+	def requestClose(self):
+		if self.plugins_changed:
+			plugins.readPluginList(resolveFilename(SCOPE_PLUGINS))
+		if self.reload_settings:
+			self["text"].setText(_("Reloading bouquets and services..."))
+			eDVBDB.getInstance().reloadBouquets()
+			eDVBDB.getInstance().reloadServicelist()
+		plugins.readPluginList(resolveFilename(SCOPE_PLUGINS))
+		self.container.appClosed.remove(self.runFinished)
+		self.container.dataAvail.remove(self.dataAvail)
+		self.close()
+
+	def resetPostInstall(self):
+		try:
+			del self.postInstallCall
+		except:
+			pass
+
+	def installDestinationCallback(self, result):
+		if result is not None:
+			dest = result[1]
+			if dest.startswith('/'):
+				# Custom install path, add it to the list too
+				dest = os.path.normpath(dest)
+				extra = '--add-dest %s:%s -d %s' % (dest,dest,dest)
+				Ipkg.opkgAddDestination(dest)
+			else:
+				extra = '-d ' + dest
+			self.doInstall(self.installFinished, pluginnames + ' ' + extra)
+		else:
+			self.resetPostInstall()
+
+	def runInstall(self, val):
+		if val:
+			if self.type == self.DOWNLOAD:
+				if pluginnames.startswith("enigma2-plugin-picons-"):
+					supported_filesystems = frozenset(('vfat','ext4', 'ext3', 'ext2', 'reiser', 'reiser4', 'jffs2', 'ubifs', 'rootfs'))
+					candidates = []
+					import Components.Harddisk
+					mounts = Components.Harddisk.getProcMounts()
+					for partition in harddiskmanager.getMountedPartitions(False, mounts):
+						if partition.filesystem(mounts) in supported_filesystems:
+							candidates.append((partition.description, partition.mountpoint))
+					if candidates:
+						from Components.Renderer import Picon
+						self.postInstallCall = Picon.initPiconPaths
+						self.session.openWithCallback(self.installDestinationCallback, ChoiceBox, title=_("Install picons on"), list=candidates)
+					return
+				elif pluginnames.startswith("enigma2-plugin-display-picon"):
+					supported_filesystems = frozenset(('vfat','ext4', 'ext3', 'ext2', 'reiser', 'reiser4', 'jffs2', 'ubifs', 'rootfs'))
+					candidates = []
+					import Components.Harddisk
+					mounts = Components.Harddisk.getProcMounts()
+					for partition in harddiskmanager.getMountedPartitions(False, mounts):
+						if partition.filesystem(mounts) in supported_filesystems:
+							candidates.append((partition.description, partition.mountpoint))
+					if candidates:
+						from Components.Renderer import LcdPicon
+						self.postInstallCall = LcdPicon.initLcdPiconPaths
+						self.session.openWithCallback(self.installDestinationCallback, ChoiceBox, title=_("Install lcd picons on"), list=candidates)
+					return
+				self.install_settings_name = pluginnames
+				self.install_bootlogo_name = pluginnames
+				if pluginnames.startswith('enigma2-plugin-settings-'):
+					self.check_settings = True
+					self.startIpkgListInstalled(self.PLUGIN_PREFIX + 'settings-*')
+				elif pluginnames.startswith('enigma2-plugin-bootlogo-'):
+					self.check_bootlogo = True
+					self.startIpkgListInstalled(self.PLUGIN_PREFIX + 'bootlogo-*')
+				else:
+					self.runSettingsInstall()
+
+	def doInstall(self, callback, pkgname):
+	        if "mgcamd" in pkgname or "scam" in pkgname or "gbox" in pkgname:
+		        self.session.openWithCallback(callback, Console, cmdlist = [self.ipkg_install1 + " " + self.PLUGIN_PREFIX + pkgname, "sync"], closeOnSuccess = True)	        
+	        else: 
+		        self.session.openWithCallback(callback, Console, cmdlist = [self.ipkg_install + " " + self.PLUGIN_PREFIX + pkgname, "sync"], closeOnSuccess = True)
+
+	def runSettingsInstall(self):
+		self.doInstall(self.installFinished, self.install_settings_name)
+
+	def ConvertSize(self, size):
+		size = int(size)
+		if size >= 1073741824:
+			Size = '%0.2f TB' % (size / 1073741824.0)
+		elif size >= 1048576:
+			Size = '%0.2f GB' % (size / 1048576.0)
+		elif size >= 1024:
+			Size = '%0.2f MB' % (size / 1024.0)
+		else:
+			Size = '%0.2f KB' % size
+		return str(Size)
+
+	def setWindowTitle(self):
+		diskSpace = getVarSpaceKb()
+		percFree = int(diskSpace[0] / diskSpace[1] * 100)
+		percUsed = int((diskSpace[1] - diskSpace[0]) / diskSpace[1] * 100)
+		self.setTitle('%s - %s: %s (%d%%)' % (_('Install plugins'),
+		 _('Free'),
+		 self.ConvertSize(int(diskSpace[0])),
+		 percFree))
+		self['spaceused'].setValue(percUsed)
+		
+	def startIpkgListInstalled(self, pkgname = PLUGIN_PREFIX + '*'):
+		self.container.execute(self.ipkg + Ipkg.opkgExtraDestinations() + " list_installed '%s'" % pkgname)
+
+	def startIpkgListAvailable(self):
+		self.container.execute(self.ipkg + Ipkg.opkgExtraDestinations() + " list '" + self.PLUGIN_PREFIX + "*'")
+
+	def startRun(self):
+		listsize = self["list"].instance.size()
+		self["list"].instance.hide()
+		self.listWidth = listsize.width()
+		self.listHeight = listsize.height()
+		if self.type == self.DOWNLOAD:
+			self.container.execute(self.ipkg + " update")
+
+	def installFinished(self):
+		if hasattr(self, 'postInstallCall'):
+			try:
+				self.postInstallCall()
+			except Exception, ex:
+				print "[PluginBrowser] postInstallCall failed:", ex
+			self.resetPostInstall()
+		try:
+			os.unlink('/tmp/opkg.conf')
+		except:
+			pass
+		for plugin in self.pluginlist:
+			if plugin[3] == pluginnames:
+				self.pluginlist.remove(plugin)
+				break
+		self.plugins_changed = True
+		if pluginnames.startswith("enigma2-plugin-settings-"):
+			self.reload_settings = True
+		self.expanded = []
+		self.updateList()
+		self["list"].moveToIndex(0)
+
+	def runFinished(self, retval):
+		if self.check_settings:
+			self.check_settings = False
+			self.runSettingsInstall()
+			return
+		if self.check_bootlogo:
+			self.check_bootlogo = False
+			self.runSettingsInstall()
+			return
+		self.remainingdata = ""
+		if self.run == 0:
+			self.run = 1
+			if self.type == self.DOWNLOAD:
+				self.startIpkgListInstalled()
+		elif self.run == 1 and self.type == self.DOWNLOAD:
+			self.run = 2
+			from Components import opkg
+			pluginlist = []
+			self.pluginlist = pluginlist
+			for plugin in opkg.enumPlugins(self.PLUGIN_PREFIX):
+				if plugin[0] not in self.installedplugins:
+					pluginlist.append(plugin + (plugin[0][15:],))
+			if pluginlist:
+				pluginlist.sort()
+				self.updateList()
+				self["list"].instance.show()
+			else:
+				self["text"].setText(_("No new plugins found"))
+		else:
+			if self.pluginlist:
+				self.updateList()
+				self["list"].instance.show()
+			else:
+				if self.type == self.DOWNLOAD:
+					self["text"].setText(_("Sorry feeds are down for maintenance"))
+
+	def dataAvail(self, str):
+		if self.type == self.DOWNLOAD and ('wget returned 1' or 'wget returned 255' or '404 Not Found') in str:
+			self.run = 3
+			return
+
+		#prepend any remaining data from the previous call
+		str = self.remainingdata + str
+		#split in lines
+		lines = str.split('\n')
+		#'str' should end with '\n', so when splitting, the last line should be empty. If this is not the case, we received an incomplete line
+		if len(lines[-1]):
+			#remember this data for next time
+			self.remainingdata = lines[-1]
+			lines = lines[0:-1]
+		else:
+			self.remainingdata = ""
+
+		if self.check_settings:
+			self.check_settings = False
+			self.remove_settings_name = str.split(' - ')[0].replace(self.PLUGIN_PREFIX, '')
+			self.session.openWithCallback(self.runSettingsRemove, MessageBox, _('You already have a channel list installed,\nwould you like to remove\n"%s"?') % self.remove_settings_name)
+			return
+
+		if self.check_bootlogo:
+			self.check_bootlogo = False
+			self.remove_bootlogo_name = str.split(' - ')[0].replace(self.PLUGIN_PREFIX, '')
+			self.session.openWithCallback(self.runBootlogoRemove, MessageBox, _('You already have a bootlogo installed,\nwould you like to remove\n"%s"?') % self.remove_bootlogo_name)
+			return
+
+		if self.run == 1:
+			for x in lines:
+				plugin = x.split(" - ", 2)
+				# 'opkg list_installed' only returns name + version, no description field
+				if len(plugin) >= 2:
+					if not plugin[0].endswith('-dev') and not plugin[0].endswith('-staticdev') and not plugin[0].endswith('-dbg') and not plugin[0].endswith('-doc'):
+						if plugin[0] not in self.installedplugins:
+							if self.type == self.DOWNLOAD:
+								self.installedplugins.append(plugin[0])
+							else:
+								if len(plugin) == 2:
+									plugin.append('')
+								plugin.append(plugin[0][15:])
+								self.pluginlist.append(plugin)
+			self.pluginlist.sort()
+
+	def updateList(self):
+		list = []
+		expandableIcon = LoadPixmap(resolveFilename(SCOPE_ACTIVE_SKIN, "icons/expandable-plugins.png"))
+		expandedIcon = LoadPixmap(resolveFilename(SCOPE_ACTIVE_SKIN, "icons/expanded-plugins.png"))
+		verticallineIcon = LoadPixmap(resolveFilename(SCOPE_ACTIVE_SKIN, "icons/verticalline-plugins.png"))
+
+		self.plugins = {}
+		for x in self.pluginlist:
+			split = x[3].split('-', 1)
+			if len(split) < 2:
+				continue
+			if not self.plugins.has_key(split[0]):
+				self.plugins[split[0]] = []
+
+			self.plugins[split[0]].append((PluginDescriptor(name = x[3], description = x[2], icon = verticallineIcon), split[1], x[1]))
+
+		temp = self.plugins.keys()
+		if config.usage.sort_pluginlist.value:
+			temp.sort()
+		for x in temp:
+			if x in self.expanded:
+				list.append(PluginCategoryComponent(x, expandedIcon, self.listWidth))
+				list.extend([PluginDownloadComponent(plugin[0], plugin[1], plugin[2], self.listWidth) for plugin in self.plugins[x]])
+			else:
+				list.append(PluginCategoryComponent(x, expandableIcon, self.listWidth))
+		self.list = list
+		self["list"].l.setList(list)
+		self["text"] = Label(_("Downloading plugin information complete."))
