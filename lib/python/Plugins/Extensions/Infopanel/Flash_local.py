197b2df948680f23e6905b2553a0f00c341a1a2b
torwald12 <torwald12@team.nachtfalke.biz>
2018-04-17 22:57:34 +0200

    add u51

--- lib/python/Plugins/Extensions/Infopanel/Flash_local.py
+++ lib/python/Plugins/Extensions/Infopanel/Flash_local.py
@@ -525,7 +525,7 @@ class doFlashImage(Screen):
 				text += _("Simulate (no write)")
 				if SystemInfo["HaveMultiBoot"]:
 					cmdlist.append("%s -n -r -k -m%s %s > /dev/null 2>&1" % (ofgwritePath, self.multi, flashTmp))
-				elif getMachineBuild() in ("u52","u53","u5","u5pvr"):
+				elif getMachineBuild() in ("u51","u52","u53","u5","u5pvr"):
  					cmdlist.append("%s -n -r%s -k%s %s > /dev/null 2>&1" % (ofgwritePath, MTDROOTFS, MTDKERNEL, flashTmp))					
 				else:
 					cmdlist.append("%s -n -r -k %s > /dev/null 2>&1" % (ofgwritePath, flashTmp))
@@ -542,7 +542,7 @@ class doFlashImage(Screen):
 					if not self.List == "STARTUP":
 						cmdlist.append("umount -fl /oldroot_bind")
 						cmdlist.append("umount -fl /newroot")
-				elif getMachineBuild() in ("u52","u53","u5","u5pvr"):
+				elif getMachineBuild() in ("u51","u52","u53","u5","u5pvr"):
 	 				cmdlist.append("%s -r%s -k%s %s > /dev/null 2>&1" % (ofgwritePath, MTDROOTFS, MTDKERNEL, flashTmp))					
 				else:
 					cmdlist.append("%s -r -k %s > /dev/null 2>&1" % (ofgwritePath, flashTmp))
