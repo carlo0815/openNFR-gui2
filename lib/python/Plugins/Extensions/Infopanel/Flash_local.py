bbcf89e7cc63c4857a255a1f10d29f9eb0ea79b9
torwald12 <torwald12@team.nachtfalke.biz>
2018-11-08 20:43:11 +0100

    add hd60

--- lib/python/Plugins/Extensions/Infopanel/Flash_local.py
+++ lib/python/Plugins/Extensions/Infopanel/Flash_local.py
@@ -574,6 +574,8 @@ class doFlashImage(Screen):
 						cmdlist.append("%s -n -r -k -m%s %s > /dev/null 2>&1" % (ofgwritePath, self.multi, flashTmp))
 				elif getMachineBuild() in ("u51","u52","u53","u5","u5pvr","cc1","sf8008"):
  					cmdlist.append("%s -n -r%s -k%s %s > /dev/null 2>&1" % (ofgwritePath, MTDROOTFS, MTDKERNEL, flashTmp))					
+				elif getMachineBuild() in ("h9","i55plus","hd60","v8plus"):
+					cmdlist.append("%s -n -f -r -k %s > /dev/null 2>&1" % (ofgwritePath, flashTmp))
 				else:
 					cmdlist.append("%s -n -r -k %s > /dev/null 2>&1" % (ofgwritePath, flashTmp))
 				self.close()
@@ -594,6 +596,8 @@ class doFlashImage(Screen):
 						cmdlist.append("umount -fl /newroot")
 				elif getMachineBuild() in ("u51","u52","u53","u5","u5pvr","cc1","sf8008"):
 	 				cmdlist.append("%s -r%s -k%s %s > /dev/null 2>&1" % (ofgwritePath, MTDROOTFS, MTDKERNEL, flashTmp))					
+				elif getMachineBuild() in ("h9","i55plus","hd60","v8plus"):
+					cmdlist.append("%s -f -r -k %s > /dev/null 2>&1" % (ofgwritePath, flashTmp))
 				else:
 					cmdlist.append("%s -r -k %s > /dev/null 2>&1" % (ofgwritePath, flashTmp))
 				message = "echo -e '\n"
