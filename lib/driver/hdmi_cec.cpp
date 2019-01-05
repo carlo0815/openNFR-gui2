2c3dc38f7b53fd053ffad002d5ad79c79afcb474
torwald12 <torwald12@team.nachtfalke.biz>
2019-01-05 18:39:39 +0100

    Update hdmi_cec.cpp

--- lib/driver/hdmi_cec.cpp
+++ lib/driver/hdmi_cec.cpp
@@ -373,7 +373,7 @@ void eHdmiCEC::hdmiEvent(int what)
 			bool keypressed = false;
 			static unsigned char pressedkey = 0;
 
-			eDebugNoNewLineStart("[eHdmiCEC] received message");
+			eDebug("[eHdmiCEC] received message");
 			eDebugNoNewLine(" %02X", rxmessage.address);
 			for (int i = 0; i < rxmessage.length; i++)
 			{
@@ -527,7 +527,7 @@ void eHdmiCEC::sendMessage(struct cec_message &message)
 {
 	if (hdmiFd >= 0)
 	{
-		eDebugNoNewLineStart("[eHdmiCEC] send message");
+		eDebug("[eHdmiCEC] send message");
 		eDebugNoNewLine(" %02X", message.address);
 		for (int i = 0; i < message.length; i++)
 		{
