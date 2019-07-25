a42eda3197ce1f592db110d11e7c2ab85ca1ca1a
toto <toto_golden@gmx.de>
2019-07-25 15:37:33 +0200

    fix wrong Directory

--- lib/python/Plugins/Extensions/FileLoad/plugin.py
+++ lib/python/Plugins/Extensions/FileLoad/plugin.py
@@ -1,6 +1,6 @@
 from Plugins.Plugin import PluginDescriptor
 import subprocess
-cmd = ['python','-O','/usr/lib/enigma2/python/Plugins/Extensions/upload/upload.py']
+cmd = ['python','-O','/usr/lib/enigma2/python/Plugins/Extensions/FileLoad/upload.py']
 extScript = subprocess.Popen(cmd,shell=False,stdout=subprocess.PIPE)
         
 def Plugins(**kwargs):
