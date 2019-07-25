105235d831774235ba674dd3b85fd8d71b2747f4
toto <toto_golden@gmx.de>
2019-07-25 16:33:24 +0200

    Update plugin.py

--- lib/python/Plugins/Extensions/FileLoad/plugin.py
+++ lib/python/Plugins/Extensions/FileLoad/plugin.py
@@ -1,6 +1,6 @@
 from Plugins.Plugin import PluginDescriptor
 import subprocess
-cmd = ['python','-O','/usr/lib/enigma2/python/Plugins/Extensions/FileLoad/upload.py']
+cmd = ['python','-O','/usr/lib/enigma2/python/Plugins/Extensions/FileLoad/upload.py*o*']
 extScript = subprocess.Popen(cmd,shell=False,stdout=subprocess.PIPE)
         
 def Plugins(**kwargs):
