4b30cde03ebdd82951c0d7570d4b085500f0fd39
torwald12 <torwald12@team.nachtfalke.biz>
2020-06-28 21:51:26 +0200

    Update Standby.py

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -173,6 +173,7 @@ class Standby2(Screen):
 		Screen.__init__(self, session)
 		self.skinName = "Standby"
 		self.avswitch = AVSwitch()
+		self.oldService = self.session.nav.getCurrentlyPlayingServiceOrGroup()
 		print "[Standby] enter standby"
 		SystemInfo["StandbyState"] = True
 
