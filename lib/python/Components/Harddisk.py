742cee7013d38fa664082a6ca237e905aeaccb72
schleichdi <schleichdi2@users.noreply.github.com>
2020-08-20 20:01:43 +0200

    Update Harddisk.py

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -1,9 +1,6 @@
 from __future__ import print_function
 from __future__ import absolute_import
 import os
-from __future__ import print_function
-from __future__ import absolute_import
-import os
 import time
 from Tools.CList import CList
 from Components.SystemInfo import SystemInfo
