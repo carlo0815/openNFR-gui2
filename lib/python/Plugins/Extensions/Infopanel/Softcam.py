54b38439a806ba3b64b5d1324e175fb9e580831b
schleichdi <schleichdi2@users.noreply.github.com>
2020-08-22 17:49:03 +0200

    Update Softcam.py

--- lib/python/Plugins/Extensions/Infopanel/Softcam.py
+++ lib/python/Plugins/Extensions/Infopanel/Softcam.py
@@ -10,26 +10,26 @@ def getcamcmd(cam):
 	camname = cam.lower()
 	xcamname = cam
 	if "gbox" in camname:
-                files=os.listdir("/lib")
+		files=os.listdir("/lib")
  		for filename in files:
-                	if filename.startswith('ld-2'):
-                		ldcheck=filename
-		symcheck = "/lib/ld-linux.so.3"
-		if os.path.islink(symcheck):
-			print("Symlink exist to start gbox")
-		else:
-			print("Create Symlink to start gbox")
-			os.system("ln -sf " + ldcheck + " '/lib/ld-linux.so.3'") 	
+			if filename.startswith('ld-2'):
+				ldcheck=filename
+				symcheck = "/lib/ld-linux.so.3"
+			if os.path.islink(symcheck):
+				print("Symlink exist to start gbox")
+			else:
+				print("Create Symlink to start gbox")
+				os.system("ln -sf " + ldcheck + " '/lib/ld-linux.so.3'")
 	if getcamscript(camname):
 		return config.NFRSoftcam.camdir.value + "/" + cam + " start"
 	elif ".x" in camname:
-	        if "mgcamd" in camname:
-	        	return config.NFRSoftcam.camdir.value + "/" + cam
-	        elif "oscam_oscamupdater" in camname:
-	                cam_name_1 = xcamname.strip(".x")
+		if "mgcamd" in camname:
+			return config.NFRSoftcam.camdir.value + "/" + cam
+		elif "oscam_oscamupdater" in camname:
+			cam_name_1 = xcamname.strip(".x")
 			return config.NFRSoftcam.camdir.value + "/" + cam + " -bc " + \
 				config.NFRSoftcam.camconfig.value + "/" + cam_name_1
-	        else:
+		else:
 			emus=[]
 			i = 0
 			for fAdd in glob ('/etc/*.emu'):
@@ -40,19 +40,19 @@ def getcamcmd(cam):
 					if "binname" in line:
 						emus.append(line[10:])
 						if cam_name in emus[i]:
-						        searchemu = open(fAdd, "r")
-					                for line in searchemu:
+							searchemu = open(fAdd, "r")
+							for line in searchemu:
 								if "startcam" in line:
-                                                			emustart.append(line[11:])
-                                                			emustart = emustart[0].strip()
-                                                			cam_count_test = emustart.count(" ")
-                                                 			start_emu = emustart.split(" ", 1 )
-                                                 			if (cam_count_test == 0):
-                                                 				return config.NFRSoftcam.camdir.value + "/" + cam
-                                                 			else: 
-                                                    				return config.NFRSoftcam.camdir.value + "/" + cam + " " + start_emu[1]
-                        	i = i + 1                        	
-				searchfile.close()
+									emustart.append(line[11:])
+									emustart = emustart[0].strip()
+									cam_count_test = emustart.count(" ")
+									start_emu = emustart.split(" ", 1 )
+									if (cam_count_test == 0):
+										return config.NFRSoftcam.camdir.value + "/" + cam
+									else:
+										return config.NFRSoftcam.camdir.value + "/" + cam + " " + start_emu[1]
+										i = i + 1
+										searchfile.close()
 			
 			for fAdd in glob ('/etc/init.d/softcam.*'):
 				emustart=[]
@@ -62,12 +62,12 @@ def getcamcmd(cam):
 				        cam_name1 = cam_name[6:]
 				        cam_scripts = "/etc/init.d/softcam." + cam_name1
 				if fAdd == cam_scripts:
-				        cam_starts = cam_scripts + " start"
-				        return cam_starts
+					cam_starts = cam_scripts + " start"
+					return cam_starts
 				else:
-                                        
-                                        print("no emustarter found in /etc/init.d please check!")
-                     	                return config.NFRSoftcam.camdir.value + "/" + cam
+
+					print("no emustarter found in /etc/init.d please check!")
+					return config.NFRSoftcam.camdir.value + "/" + cam
 			
 	
 	else:
@@ -76,7 +76,7 @@ def getcamcmd(cam):
 				config.NFRSoftcam.camconfig.value + "/"
 		if "doscam" in camname:
 			return config.NFRSoftcam.camdir.value + "/" + cam + " -bc " + \
-				config.NFRSoftcam.camconfig.value + "/"				
+				config.NFRSoftcam.camconfig.value + "/"
 		elif "wicard" in camname:
 			return "ulimit -s 512; " + config.NFRSoftcam.camdir.value + \
 			"/" + cam + " -d -c " + config.NFRSoftcam.camconfig.value + \
@@ -90,11 +90,11 @@ def getcamcmd(cam):
 		elif "cccam" in camname:
 			return config.NFRSoftcam.camdir.value + "/" + cam + " -C " + \
 				config.NFRSoftcam.camconfig.value + "/CCcam.cfg"
-                elif "mgcamd" in camname:
-	                os.system("rm /dev/dvb/adapter0/ca1")
-	                os.system("ln -sf 'ca0' '/dev/dvb/adapter0/ca1'")                 
+		elif "mgcamd" in camname:
+			os.system("rm /dev/dvb/adapter0/ca1")
+			os.system("ln -sf 'ca0' '/dev/dvb/adapter0/ca1'")
 			return config.NFRSoftcam.camdir.value + "/" + cam + " -bc " + \
-				config.NFRSoftcam.camconfig.value + "/"                				
+				config.NFRSoftcam.camconfig.value + "/"
 		elif "mpcs" in camname:
 			return config.NFRSoftcam.camdir.value + "/" + cam + " -c " + \
 				config.NFRSoftcam.camconfig.value
@@ -111,7 +111,7 @@ def getcamcmd(cam):
 				config.NFRSoftcam.camconfig.value + "/gbox.cfg"	
  		elif "scam" in camname and not "oscam" in camname:
 			return config.NFRSoftcam.camdir.value + "/" + cam + " -s " + \
-				config.NFRSoftcam.camconfig.value + "/"			
+				config.NFRSoftcam.camconfig.value + "/"
 		else:
 			return config.NFRSoftcam.camdir.value + "/" + cam
 
