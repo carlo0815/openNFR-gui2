05523e0a5d907f4357376c7720807814c7136c2a
schleichdi <schleichdi2@users.noreply.github.com>
2020-08-22 15:44:45 +0200

    Update nfsedit.py

--- lib/python/Plugins/Extensions/Infopanel/nfsedit.py
+++ lib/python/Plugins/Extensions/Infopanel/nfsedit.py
@@ -40,48 +40,48 @@ if sz_w > 800:
 else:
 	HDSkn = False
 	
-               
+
 class NFS_EDIT(Screen):
 	global HDSkn
 	if HDSkn:
 		if (getDesktop(0).size().width()) > 1030:
-                        skin = """
-			  <screen name="NFS_Edit" position="40,90" size="1180,590" title="File-Explorer">
-  <widget name="filedata" position="5,7" size="1170,527" itemHeight="25" />
-  <ePixmap position="6,559" size="35,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/red.png" alphatest="blend" />
-  <ePixmap position="252,559" size="35,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/green.png" alphatest="blend" zPosition="2" />
-  <ePixmap position="493,559" size="35,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/yellow.png" alphatest="blend" zPosition="3" />
-  <ePixmap position="724,559" size="35,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/blue.png" alphatest="blend" zPosition="4" />
-  <ePixmap position="955,557" size="70,30" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/Info.png" alphatest="blend" />
-  <widget source="key_red" render="Label" position="48,562" zPosition="2" size="190,22" valign="center" halign="center" font="Regular; 20" transparent="1" shadowColor="black" shadowOffset="-1,-1" />
-  <widget source="key_green" render="Label" position="291,562" zPosition="2" size="190,22" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" backgroundColor="foreground" />
-  <widget source="key_yellow" render="Label" position="530,562" zPosition="3" size="190,22" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" backgroundColor="foreground" />
-  <widget source="key_blue" render="Label" position="762,562" zPosition="2" size="190,22" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" backgroundColor="foreground" />
-  <widget source="key_info" render="Label" position="1052,562" zPosition="5" size="214,22" valign="center" halign="left" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" backgroundColor="foreground" />
-  <eLabel name="new eLabel" position="6,553" size="1170,2" backgroundColor="blue" foregroundColor="blue" />
-</screen>"""
+			skin = """
+			<screen name="NFS_Edit" position="40,90" size="1180,590" title="File-Explorer">
+			<widget name="filedata" position="5,7" size="1170,527" itemHeight="25" />
+			<ePixmap position="6,559" size="35,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/red.png" alphatest="blend" />
+			<ePixmap position="252,559" size="35,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/green.png" alphatest="blend" zPosition="2" />
+			<ePixmap position="493,559" size="35,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/yellow.png" alphatest="blend" zPosition="3" />
+			<ePixmap position="724,559" size="35,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/blue.png" alphatest="blend" zPosition="4" />
+			<ePixmap position="955,557" size="70,30" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/Info.png" alphatest="blend" />
+			<widget source="key_red" render="Label" position="48,562" zPosition="2" size="190,22" valign="center" halign="center" font="Regular; 20" transparent="1" shadowColor="black" shadowOffset="-1,-1" />
+			<widget source="key_green" render="Label" position="291,562" zPosition="2" size="190,22" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" backgroundColor="foreground" />
+			<widget source="key_yellow" render="Label" position="530,562" zPosition="3" size="190,22" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" backgroundColor="foreground" />
+			<widget source="key_blue" render="Label" position="762,562" zPosition="2" size="190,22" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" backgroundColor="foreground" />
+			<widget source="key_info" render="Label" position="1052,562" zPosition="5" size="214,22" valign="center" halign="left" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" backgroundColor="foreground" />
+			<eLabel name="new eLabel" position="6,553" size="1170,2" backgroundColor="blue" foregroundColor="blue" />
+			</screen>"""
 		else:
 			skin = """
 			<screen position="center,77" size="900,450" title="File-Explorer">
 				<widget name="NFS_Edit" position="2,0" size="896,450" itemHeight="25"/>
-			        <ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/red.png" position="60,200" zPosition="0" size="140,40" alphatest="on" />
-		                <ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/green.png" position="250,250" zPosition="0" size="140,40" alphatest="on" />
-		                <ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/yellow.png" position="440,250" zPosition="0" size="140,40" alphatest="on" />
-			        <widget name="key_red" position="60,200" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="yellow" />
-			        <widget name="key_green" position="250,250" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="blue" />
-			        <widget name="key_yellow" position="440,250" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="blue" />
+				<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/red.png" position="60,200" zPosition="0" size="140,40" alphatest="on" />
+				<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/green.png" position="250,250" zPosition="0" size="140,40" alphatest="on" />
+				<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/yellow.png" position="440,250" zPosition="0" size="140,40" alphatest="on" />
+				<widget name="key_red" position="60,200" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="yellow" />
+				<widget name="key_green" position="250,250" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="blue" />
+				<widget name="key_yellow" position="440,250" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="blue" />
 
 			</screen>"""
 	else:
 		skin = """
 		<screen position="center,77" size="620,450" title="File-Explorer">
 			<widget name="NFS_Edit" position="0,0" size="620,450" itemHeight="25"/>
-			        <ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/red.png" position="60,200" zPosition="0" size="140,40" alphatest="on" />
-		                <ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/green.png" position="250,250" zPosition="0" size="140,40" alphatest="on" />
-		                <ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/yellow.png" position="440,250" zPosition="0" size="140,40" alphatest="on" />
-			        <widget name="key_red" position="60,200" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="yellow" />
-			        <widget name="key_green" position="250,250" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="blue" />
-        		        <widget name="key_yellow" position="440,250" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="blue" />
+				<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/red.png" position="60,200" zPosition="0" size="140,40" alphatest="on" />
+				<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/green.png" position="250,250" zPosition="0" size="140,40" alphatest="on" />
+				<ePixmap pixmap="/usr/lib/enigma2/python/Plugins/Extensions/Infopanel/pics/yellow.png" position="440,250" zPosition="0" size="140,40" alphatest="on" />
+				<widget name="key_red" position="60,200" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="yellow" />
+				<widget name="key_green" position="250,250" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="blue" />
+				<widget name="key_yellow" position="440,250" size="140,40" zPosition="-1" valign="center" halign="center" font="Regular;20" transparent="1" backgroundColor="blue" />
 
 		</screen>"""
 
@@ -94,18 +94,18 @@ class NFS_EDIT(Screen):
 		self.list = []
 		self["key_red"] = StaticText(_("Cancel"))
 		self["key_green"] = StaticText(_("Save"))
-		self["key_yellow"] = StaticText(_("NewLine"))	
-		self["key_blue"] = StaticText(_("DeleteLine"))                	
+		self["key_yellow"] = StaticText(_("NewLine"))
+		self["key_blue"] = StaticText(_("DeleteLine"))
 		self["filedata"] = MenuList(self.list)
 		self["actions"] = ActionMap(["WizardActions", "ColorActions", "EPGSelectActions"],
 
 		{
-		    "ok": self.editLine,
-                    "back": self.exitEditor,
-		    "yellow": self.yellow, 
-		    "blue": self.blue,                                       
-		    "green": self.green,
-		    "red": self.close,
+			"ok": self.editLine,
+			"back": self.exitEditor,
+			"yellow": self.yellow, 
+			"blue": self.blue
+			"green": self.green,
+			"red": self.close,
 
 		}, -1)
 		self.selLine = None
@@ -115,11 +115,11 @@ class NFS_EDIT(Screen):
 		if fileExists('/etc/exports'):
 			self.GetFileData(file)
 		else: 
-                	os.system('echo "/media/hdd 192.168.0.0/255.255.255.0(rw,all_squash,anonuid=0,anongid=0,async)">/etc/exports')
-		        self.GetFileData(file)
-		        
+			os.system('echo "/media/hdd 192.168.0.0/255.255.255.0(rw,all_squash,anonuid=0,anongid=0,async)">/etc/exports')
+			self.GetFileData(file)
+
 	def green(self):
-        	self.SaveFile(True)		
+		self.SaveFile(True)
 		
 	def exitEditor(self):
 		if self.isChanged:
@@ -151,15 +151,15 @@ class NFS_EDIT(Screen):
 		
 	def blue(self):
 		self.selLine = self["filedata"].getSelectionIndex()
-                i = int(self.selLine) 
-                li = i - 1
-                lr = i + 2		
-                #for x in self.list[li:lr]:
-                #         self.isChanged = True
-                del self.list[i]
+		i = int(self.selLine) 
+		li = i - 1
+		lr = i + 2
+		#for x in self.list[li:lr]:
+		#         self.isChanged = True
+		del self.list[i]
 		self.selLine = None
 		self.session.open(MessageBox, _("Select Line is deleted!"), MessageBox.TYPE_INFO, timeout=3)
-		#self.SaveFile(True)		
+		#self.SaveFile(True)
 
 	def editLine(self):
 		try:
@@ -173,9 +173,9 @@ class NFS_EDIT(Screen):
 
 
 	def callbackEditLine(self, newline):
-                i = int(self.selLine) 
-                li = i - 1
-                lr = i + 2
+		i = int(self.selLine) 
+		li = i - 1
+		lr = i + 2
 		if newline is not None:
 			for x in self.list[li:lr]:
 				if x == self.oldLine:
