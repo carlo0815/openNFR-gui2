0f9ebb77c2ad84729f1cfcd886407b02832ee747
n3wb13 <michael.gaedke@gmx.net>
2020-08-23 12:49:17 +0200

    Update PluginBrowser.py

--- lib/python/Screens/PluginBrowser.py
+++ lib/python/Screens/PluginBrowser.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from Screens.ParentalControlSetup import ProtectedScreen
 from boxbranding import getImageVersion
-from urllib import urlopen
+from six.moves import urllib
 import socket
 import os
 
