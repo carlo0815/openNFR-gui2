452dbeed5279e7b64160b3fd783a1821d194e8ba
toto <toto_golden@gmx.de>
2020-08-31 19:14:25 +0200

    next Fix
    commit 7b28a9073bb7aa0dce9c6982866d2690a5461b67
    committed on 30 Jun
    Fix restore crash (thanks Beta)

--- lib/python/Screens/PictureInPicture.py
+++ lib/python/Screens/PictureInPicture.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, division
 from Screens.Screen import Screen
 from Screens.Dish import Dishpip
 from enigma import ePoint, eSize, eRect, eServiceCenter, getBestPlayableServiceReference, eServiceReference, eTimer
