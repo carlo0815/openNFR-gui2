f780f3a951cb70156b8fd19f209118ced6344488
toto <toto_golden@gmx.de>
2020-09-13 08:34:13 -0400

    Add files via upload

--- lib/python/Components/ScrollLabel.py
+++ lib/python/Components/ScrollLabel.py
@@ -6,138 +6,154 @@ from Components.GUIComponent import GUIComponent
 from enigma import eLabel, eWidget, eSlider, fontRenderClass, ePoint, eSize
 
 class ScrollLabel(HTMLComponent, GUIComponent):
-	def __init__(self, text=""):
+	def __init__(self, text="", showscrollbar=True):
 		GUIComponent.__init__(self)
 		self.message = text
+		self.showscrollbar=showscrollbar
 		self.instance = None
 		self.long_text = None
 		self.right_text = None
 		self.scrollbar = None
-		self.pages = None
-		self.total = None
+		self.TotalTextHeight = 0
+		self.curPos = 0
+		self.pageHeight = 0
+		self.column = 0
 		self.split = False
 		self.splitchar = "|"
-		self.column = 0
+		self.lineheight = None
+		self.scrollbarmode = "showOnDemand"
 
 	def applySkin(self, desktop, parent):
+		scrollbarWidth = 10
+		itemHeight = 30
+		scrollbarBorderWidth = 1
 		ret = False
-		if self.skinAttributes is not None:
-			widget_attribs = [ ]
-			scrollbar_attribs = [ ]
+		if self.skinAttributes:
+			widget_attribs = []
+			scrollbar_attribs = []
+			remove_attribs = [ ]
 			for (attrib, value) in self.skinAttributes:
+				if "itemHeight" in attrib:
+					itemHeight = int(value)
+					remove_attribs.append((attrib, value))
+				if "scrollbarMode" in attrib:
+					self.scrollbarmode = value
+					remove_attribs.append((attrib, value))
 				if "borderColor" in attrib or "borderWidth" in attrib:
 					scrollbar_attribs.append((attrib, value))
 				if "transparent" in attrib or "backgroundColor" in attrib:
 					widget_attribs.append((attrib, value))
+				if "scrollbarSliderForegroundColor" in attrib:
+					scrollbar_attribs.append((attrib, value))
+					remove_attribs.append((attrib, value))
+				if "scrollbarSliderBorderColor" in attrib:
+					scrollbar_attribs.append((attrib, value))
+					remove_attribs.append((attrib, value))
+				if "scrollbarSliderPicture" in attrib:
+					scrollbar_attribs.append((attrib, value))
+					remove_attribs.append((attrib, value))
+				if "scrollbarBackgroundPicture" in attrib:
+					scrollbar_attribs.append((attrib, value))
+					remove_attribs.append((attrib, value))
+				if "scrollbarWidth" in attrib:
+					scrollbarWidth = int(value)
+					remove_attribs.append((attrib, value))
+				if "scrollbarSliderBorderWidth" in attrib:
+					scrollbarBorderWidth = int(value)
+					remove_attribs.append((attrib, value))
 				if "split" in attrib:
 					self.split = int(value)
 					if self.split:
 						self.right_text = eLabel(self.instance)
+					self.skinAttributes.remove((attrib, value))	
 				if "colposition" in attrib:
 					self.column = int(value)
 				if "dividechar" in attrib:
 					self.splitchar = value
+			for (attrib, value) in remove_attribs:
+				self.skinAttributes.remove((attrib, value))
 			if self.split:
 				skin.applyAllAttributes(self.long_text, desktop, self.skinAttributes + [("halign", "left")], parent.scale)
-				skin.applyAllAttributes(self.right_text, desktop, self.skinAttributes + [("transparent", "1"), ("halign", "left" and self.column or "right")], parent.scale)
+				skin.applyAllAttributes(self.right_text, desktop, self.skinAttributes + [("transparent", "1"), ("halign", "left" if self.column else "right")], parent.scale)
 			else:
 				skin.applyAllAttributes(self.long_text, desktop, self.skinAttributes, parent.scale)
 			skin.applyAllAttributes(self.instance, desktop, widget_attribs, parent.scale)
-			skin.applyAllAttributes(self.scrollbar, desktop, scrollbar_attribs+widget_attribs, parent.scale)
+			skin.applyAllAttributes(self.scrollbar, desktop, scrollbar_attribs + widget_attribs, parent.scale)
 			ret = True
-		s = self.long_text.size()
+		self.pageWidth = self.long_text.size().width()
+		self.lineheight = fontRenderClass.getInstance().getLineHeight(self.long_text.getFont()) or itemHeight # assume a random lineheight if nothing is visible
+		lines = int(self.long_text.size().height() / self.lineheight)
+		self.pageHeight = int(lines * self.lineheight)
 		self.instance.move(self.long_text.position())
-		lineheight=fontRenderClass.getInstance().getLineHeight( self.long_text.getFont() )
-		if not lineheight:
-			lineheight = 30 # assume a random lineheight if nothing is visible
-		lines = int(s.height() / lineheight)
-		self.pageHeight = int(lines * lineheight)
-		self.instance.resize(eSize(s.width(), self.pageHeight+ int(lineheight/6)))
-		self.scrollbar.move(ePoint(s.width()-10, 0))
-		self.scrollbar.resize(eSize(10, self.pageHeight+ int(lineheight/6)))
+		self.instance.resize(eSize(self.pageWidth, self.pageHeight + int(self.lineheight/6)))
+		self.scrollbar.move(ePoint(self.pageWidth-scrollbarWidth, 0))
+		self.scrollbar.resize(eSize(scrollbarWidth, self.pageHeight+ int(self.lineheight/6)))
 		self.scrollbar.setOrientation(eSlider.orVertical)
 		self.scrollbar.setRange(0, 100)
-		self.scrollbar.setBorderWidth(1)
-		self.long_text.move(ePoint(0, 0))
-		self.long_text.resize(eSize(s.width()-30, self.pageHeight))
-		if self.split:
-			self.right_text.move(ePoint(self.column,0))
-			self.right_text.resize(eSize(s.width()-self.column-30, self.pageHeight))
+		self.scrollbar.setBorderWidth(scrollbarBorderWidth)
 		self.setText(self.message)
 		return ret
 
-	def setText(self, text):
+	def setPos(self, pos):
+		self.curPos = max(0, min(pos, self.TotalTextHeight - self.pageHeight))
+		self.long_text.move(ePoint(0, -self.curPos))
+		self.split and self.right_text.move(ePoint(self.column, -self.curPos))
+
+	def setText(self, text, showBottom=False):
 		self.message = text
-		if self.long_text is not None and self.pageHeight:
-			self.long_text.move(ePoint(0,0))
+		text = text.rstrip()
+		if self.pageHeight:
 			if self.split:
 				left = []
 				right = []
-				for line in self.message.split("\n"):
-					line = line.split(self.splitchar,1)
-					if len(line) == 1:
-						line.append("")
+				for line in text.split("\n"):
+					line = line.split(self.splitchar, 1)
 					left.append(line[0])
-					right.append(line[1].lstrip(' '))
+					right.append("" if len(line) < 2 else line[1].lstrip())
 				self.long_text.setText("\n".join(left))
 				self.right_text.setText("\n".join(right))
 			else:
-				self.long_text.setText(self.message)
-			text_height=self.long_text.calculateSize().height()
-			total=self.pageHeight
-			pages=1
-			while total < text_height:
-				total += self.pageHeight
-				pages += 1
-			s = self.long_text.size()
-			self.long_text.resize(eSize(s.width(), total))
-			if self.split:
-				self.right_text.resize(eSize(s.width()-self.column-30, total))
-			if pages > 1:
+				self.long_text.setText(text)
+			self.TotalTextHeight = self.long_text.calculateSize().height()
+			self.long_text.resize(eSize(self.pageWidth - 30, self.TotalTextHeight))
+			self.split and self.right_text.resize(eSize(self.pageWidth - self.column - 30, self.TotalTextHeight))
+			if showBottom:
+				self.lastPage()
+			else:
+				self.setPos(0)
+			if (self. scrollbarmode == "showAlways") or ((self.scrollbarmode == "showOnDemand") and self.showscrollbar and self.TotalTextHeight > self.pageHeight):
 				self.scrollbar.show()
-				self.total = total
-				self.pages = pages
 				self.updateScrollbar()
 			else:
 				self.scrollbar.hide()
-				self.total = None
-				self.pages = None
 
-	def appendText(self, text):
-		old_text = self.getText()
-		if len(str(old_text)) >0:
-			self.message += text
+	def appendText(self, text, showBottom=True):
+		if isinstance(s, text):
+			text = text
 		else:
-			self.message = text
-		if self.long_text is not None:
-			self.long_text.setText(self.message)
-			text_height=self.long_text.calculateSize().height()
-			total=self.pageHeight
-			pages=1
-			while total < text_height:
-				total += self.pageHeight
-				pages += 1
-			s = self.long_text.size()
-			self.long_text.resize(eSize(s.width(), total))
-			if self.split:
-				self.right_text.resize(eSize(s.width()-self.column-30, total))
-			if pages > 1:
-				self.scrollbar.show()
-				self.total = total
-				self.pages = pages
-				self.updateScrollbar()
-			else:
-				self.scrollbar.hide()
-				self.total = None
-				self.pages = None
+			text = text.decode()
+		self.setText(self.message + text, showBottom)
+
+	def pageUp(self):
+		if self.TotalTextHeight > self.pageHeight:
+			self.setPos(self.curPos - self.pageHeight)
+			self.updateScrollbar()
+
+	def pageDown(self):
+		if self.TotalTextHeight > self.pageHeight:
+			self.setPos(self.curPos + self.pageHeight)
+			self.updateScrollbar()
+
+	def lastPage(self):
+		self.setPos(self.TotalTextHeight-self.pageHeight)
+
+	def isAtLastPage(self):
+		return self.TotalTextHeight <= self.pageHeight or self.curPos == self.TotalTextHeight - self.pageHeight
 
 	def updateScrollbar(self):
-		start = -self.long_text.position().y() * 100 // self.total
-		vis = self.pageHeight * 100 // self.total
-		self.scrollbar.setStartEnd(start, start+vis)
-
-	def getText(self):
-		return self.message
+		vis = max(100 * self.pageHeight // self.TotalTextHeight, 3)
+		start = (100 - vis) * self.curPos // (self.TotalTextHeight - self.pageHeight)
+		self.scrollbar.setStartEnd(start, start + vis)
 
 	def GUIcreate(self, parent):
 		self.instance = eWidget(parent)
@@ -150,35 +166,8 @@ class ScrollLabel(HTMLComponent, GUIComponent):
 		self.instance = None
 		self.right_text = None
 
-	def pageUp(self):
-		if self.total is not None:
-			curPos = self.long_text.position()
-			if curPos.y() < 0:
-				self.long_text.move( ePoint( curPos.x(), curPos.y() + self.pageHeight ) )
-				self.split and self.right_text.move( ePoint( curPos.x(), curPos.y() + self.pageHeight ) )
-				self.updateScrollbar()
-
-	def pageDown(self):
-		if self.total is not None:
-			curPos = self.long_text.position()
-			if self.total-self.pageHeight >= abs( curPos.y() - self.pageHeight ):
-				self.long_text.move( ePoint( curPos.x(), curPos.y() - self.pageHeight ) )
-				self.split and self.right_text.move( ePoint( curPos.x(), curPos.y() - self.pageHeight ) )
-				self.updateScrollbar()
-
-	def lastPage(self):
-		if self.pages is not None:
-			i = 1
-			while i < self.pages:
-				self.pageDown()
-				i += 1
-
-	def isAtLastPage(self):
-		if self.total is not None:
-			curPos = self.long_text.position()
-			return self.total - self.pageHeight < abs( curPos.y() - self.pageHeight )
-		else:
-			return True
-
 	def produceHTML(self):
-		return self.getText()
+		return self.message
+
+	def getText(self):
+		return self.message
