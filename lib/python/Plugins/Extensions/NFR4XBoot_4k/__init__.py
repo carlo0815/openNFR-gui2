37545b07da783b78d09c4e63399ddd992e410a1d
toto <toto_golden@gmx.de>
2020-12-21 19:28:56 +0100

    delete no needed files
    delete no needed files

--- lib/python/Plugins/Extensions/NFR4XBoot_4k/__init__.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system("touch /tmp/.nfr4x.inf")
--- lib/python/Plugins/Extensions/NFR4XBoot_sh4/ex_init.py
+++
@@ -1,5 +0,0 @@
-import sys, nfr4xboot
-if len(sys.argv) < 5:
-    pass
-else:
-    nfr4xboot.NFR4XBootMainEx(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
