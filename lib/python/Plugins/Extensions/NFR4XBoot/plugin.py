70f5052bcf4b4e6ded0360e253e2e0d5f9f01404
toto <toto_golden@gmx.de>
2020-12-22 21:59:12 +0100

    Update plugin.py

--- lib/python/Plugins/Extensions/NFR4XBoot/plugin.py
+++ lib/python/Plugins/Extensions/NFR4XBoot/plugin.py
@@ -548,7 +548,7 @@ class NFR4XBootImageChoose(Screen):
 		else:
 			menulist = []
 			menulist.append((_('Install from /media/nfr4xboot/NFR4XBootUpload'), 'media'))
-			menulist.append((_('Install from Internet (OpenPLi,OpenVix,OpenATV,Egami,OpenNFR,OpenHDF)'), 'internet'))
+			menulist.append((_('Install from Internet (OpenVix,OpenATV,OpenNFR,OpenHDF)'), 'internet'))
 			self.session.openWithCallback(self.menuCallback, ChoiceBox, title=_('Choose they way for installation!'), list=menulist)
 
 	def menuCallback(self, choice):
