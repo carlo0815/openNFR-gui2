da0be0278299256833613d27d734cbbc6b73eb6a
torwald12 <torwald12@team.nachtfalke.biz>
2021-08-06 21:27:15 +0200

    Update bsod.cpp

--- main/bsod.cpp
+++ main/bsod.cpp
@@ -16,7 +16,7 @@
 
 static const char *crash_emailaddr =
 #ifndef CRASH_EMAILADDR
-	"the openATV forum";
+	"the openNFR forum";
 #else
 	CRASH_EMAILADDR;
 #endif
@@ -190,7 +190,7 @@ void bsodFatal(const char *component)
 		strftime(tm_str, sizeof(tm_str), "%a %b %_d %T %Y", &tm);
 
 		fprintf(f,
-			"openATV Enigma2 crash log\n\n"
+			"openNFR Enigma2 crash log\n\n"
 			"crashdate=%s\n"
 			"compiledate=%s\n"
 			"skin=%s\n"
