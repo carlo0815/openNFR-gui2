d2f8b731a7e39a94d90118c338cc99b13b0f67e4
toto <toto_golden@gmx.de>
2021-08-28 15:01:14 +0200

    Update PliExtraInfo.py

--- lib/python/Components/Converter/PliExtraInfo.py
+++ lib/python/Components/Converter/PliExtraInfo.py
@@ -273,7 +273,7 @@ class PliExtraInfo(Poll, Converter):
 
 	def createOrbPosOrTunerSystem(self, fedata, feraw):
 		orbpos = self.createOrbPos(feraw)
-		if orbpos is not "":
+		if orbpos is != "":
 			return orbpos
 		return self.createTunerSystem(fedata)
 
