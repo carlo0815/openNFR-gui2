0c1ad7f58dde0dc41ca5fe627beeb4d67bb4ee82
toto <toto_golden@gmx.de>
2022-01-29 20:48:38 +0100

    fix start compilted py

--- lib/python/Plugins/Extensions/FileLoad/plugin.py
+++ lib/python/Plugins/Extensions/FileLoad/plugin.py
@@ -2,8 +2,11 @@ from Plugins.Plugin import PluginDescriptor
 import subprocess
 cmd = ['python3','-O','/usr/lib/enigma2/python/Plugins/Extensions/FileLoad/upload.py']
 cmd1 = ['python3','-O','/usr/lib/enigma2/python/Plugins/Extensions/FileLoad/delete.py']
+cmd2 = ['python3','-O','/usr/lib/enigma2/python/Plugins/Extensions/FileLoad/upload.pyc']
+cmd3 = ['python3','-O','/usr/lib/enigma2/python/Plugins/Extensions/FileLoad/delete.pyc']
 extScript = subprocess.Popen(cmd,shell=False,stdout=subprocess.PIPE)
 extScript = subprocess.Popen(cmd1,shell=False,stdout=subprocess.PIPE)
-
+extScript = subprocess.Popen(cmd2,shell=False,stdout=subprocess.PIPE)
+extScript = subprocess.Popen(cmd3,shell=False,stdout=subprocess.PIPE)
 def Plugins(**kwargs):
     return PluginDescriptor()
