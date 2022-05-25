230db581b912816cb157f859002e77b74fdfae44
toto <toto_golden@gmx.de>
2022-05-25 20:56:59 +0200

    Add files via upload

--- include/libsig_comp.h
+++ include/libsig_comp.h
@@ -3,9 +3,7 @@
 
 #include <sigc++/sigc++.h>
 
-
 #define CONNECT(_signal, _slot) _signal.connect(sigc::mem_fun(*this, &_slot))
 #define CONNECT_EXTRA(_signal, _slot, extra_args...) _signal.connect(bind(sigc::mem_fun(*this, &_slot), extra_args))
-
 
 #endif // __LIBSIG_COMP_H
